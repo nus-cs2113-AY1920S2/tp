0d3d03320dd02762c0d73ff278b565a54c56e0ab
Jeremy Cheung <z5142254@ad.unsw.edu.au>
2020-03-05 00:05:45 +1100

    add Exit Help

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -34,9 +34,40 @@ public class Duke {
         System.out.println("Hello " + in.nextLine());
     }
 
-    public void exitESC(){
+    /**
+     * Exit method.
+     * Terminates the programme.
+     */
+    public static void exitESC(){
         System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
     }
+
+    /**
+     *  Prints a list of commands used in the programme.
+     */
+    public static void printHelp(){
+        String helpMessage = " Welcome to ESC.\n"
+                + "Create: ​create n/[CATEGORYNAME] \n"
+                + "E.g. ​create n/Biology.\n"
+                + "List Category: ​list category\n"
+                + "Select Category: ​select [INDEX]\n"
+                + "Eg. ​select 1\n"
+                + "Delete Category: ​delete category [INDEX]\n"
+                + "Eg. ​delete 1\n"
+                + "Add Card:​ add q/[QUESTION] a/[ANSWER]\n"
+                + "Eg. ​add q/Which year was NUS founded? a/1980\n"
+                + "List Card: ​list card\n"
+                + "Delete Card: ​delete [INDEX]\n"
+                + "Eg. ​delete 1\n"
+                + "Quiz: ​quiz\n"
+                + "Answer: ​answer​ / ​answer [INDEX]\n"
+                + "Eg. ​answer 1\n"
+                + "Help: ​help\n"
+                + "Exit: ​exit\n";
+
+        System.out.println(helpMessage);
+    }
+
 }
