bc510e628976af7f7d2d130006d2f8e17e84c3ac
lwxymere <chaoscold@hotmail.com>
2020-03-05 00:05:36 +0800

    Add commands

---
+++ src/main/java/seedu/duke/command/DeleteCommand.java
@@ -0,0 +1,12 @@
+package seedu.duke.command;
+
+public class DeleteCommand extends Command {
+    public DeleteCommand(int index) {
+
+    }
+
+    @Override
+    public void execute() {
+        return;
+    }
+}
---
+++ src/main/java/seedu/duke/command/DoneCommand.java
@@ -0,0 +1,12 @@
+package seedu.duke.command;
+
+public class DoneCommand extends Command {
+    public DoneCommand(int index) {
+
+    }
+
+    @Override
+    public void execute() {
+        return;
+    }
+}
