09779411d189513c5c520e0eee81c7d52c8eba49
jichngan <jcngan97@gmail.com>
2020-03-05 11:28:28 +0800

    Add skeleton code for TaskList.

---
+++ src/main/java/seedu/duke/TaskList.java
@@ -0,0 +1,39 @@
+package seedu.duke;
+
+import tasks.Task;
+import tasks.Assignment;
+import tasks.Event;
+
+import java.util.ArrayList;
+
+public class TaskList {
+
+    private ArrayList<Task> tasks;
+
+    /**
+     * Default constructor for TaskList class.
+     * Instantiate a new ArrayList object
+     */
+    public TaskList() {
+        this.tasks = new ArrayList<>();
+    }
+
+    /**
+     * Getter for size of ArrayList
+     * @return ArrayList size
+     */
+    public int getListSize() {
+        return tasks.size();
+    }
+
+    /**
+     * Getter for ArrayList of tasks
+     * @return ArrayList of tasks
+     */
+    public ArrayList<Task> getTaskArray() {
+        return this.tasks;
+    }
+
+
+
+}
