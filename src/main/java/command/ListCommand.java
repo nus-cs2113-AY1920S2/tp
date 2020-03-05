c918d7dc52b73ccb4fe2aff25fbffa3393721ead
jichngan <jcngan97@gmail.com>
2020-03-05 11:23:29 +0800

    Add some lines of ListCommand code. Work in progress.

--- src/main/java/command/ListCommand.java
+++ src/main/java/command/ListCommand.java
@@ -1,12 +1,33 @@
 package command;
 
-public class ListCommand extends Command {
-    public ListCommand(String listParam) {
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
+
+public class ListCommand extends Command {
+
+    private final String listParam;
+    private final String LIST_TODAY_COMMAND = "today";
+
+    public ListCommand(String listParam) {
+        this.listParam = listParam;
     }
+
 
     @Override
     public void execute() {
-        return;
+        if (listParam.equals(LIST_TODAY_COMMAND)) {
+
+        }
+
+
+    }
+
+
+    public static void main(String[] args) {
+        LocalDate myObj = LocalDate.now();
+        DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String formatDate = myObj.format(myFormatObj);
+        System.out.println(formatDate);
     }
 }
