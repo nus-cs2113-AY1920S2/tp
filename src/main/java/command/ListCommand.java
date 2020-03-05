3e3d33dfd4264e1ae53026941bbe96ce95fcc548
jichngan <jcngan97@gmail.com>
2020-03-05 21:17:32 +0800

    Add feature to list tasks within current week and next week.

--- src/main/java/command/ListCommand.java
+++ src/main/java/command/ListCommand.java
@@ -9,14 +9,25 @@ public class ListCommand extends Command {
     private static final String TODAY_COMMAND = "today";
     private static final String WEEK_COMMAND = "week";
 
+    /**
+     * Default constructor for ListCommand Class.
+     * @param listParam String containing user input on type of listing
+     */
     public ListCommand(String listParam) {
         this.listParam = listParam;
     }
 
+    /**
+     * Processes user input and lists tasks accordingly
+     * @param taskList TaskList class which handles operation involving ArrayList of tasks
+     * @param ui Ui class for displaying output of task scheduler
+     */
     @Override
     public void execute(TaskList taskList, Ui ui) {
         if (listParam.equals(TODAY_COMMAND)) {
             taskList.listTodayTasks();
+        } else if (listParam.equals(WEEK_COMMAND)) {
+            taskList.listWeekTasks();
         }
     }
 }
