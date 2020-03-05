dad9774f05a51278d1d56047f9677cd914a66797
joelczk <joelchang97@gmail.com>
2020-03-05 19:25:23 +0800

    Added JavaDocs for delete features.

--- src/main/java/command/DeleteCommand.java
+++ src/main/java/command/DeleteCommand.java
@@ -6,10 +6,19 @@ import seedu.duke.Ui;
 public class DeleteCommand extends Command {
     protected int deleteIndex;
 
+    /**
+     * Delete tasks based on specified index.
+     * @param index index of task to be deleted
+     */
     public DeleteCommand(int index) {
         this.deleteIndex = index;
     }
 
+    /**
+     * Deletes task and prints out Ui message.
+     * @param taskList ArrayList of tasks
+     * @param ui ui object representing delete message
+     */
     @Override
     public void execute(TaskList taskList, Ui ui) {
         ui.showDeleteMessage(taskList.getTask(deleteIndex), deleteIndex);
