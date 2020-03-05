c5de96f18a37a12a3c8298d8c9aac22622b54b40
joelczk <joelchang97@gmail.com>
2020-03-05 23:18:08 +0800

    Add tests

--- src/test/java/seedu/duke/TaskListTest.java
+++ src/test/java/seedu/duke/TaskListTest.java
@@ -2,6 +2,7 @@ package seedu.duke;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import tasks.Task;
 
 public class TaskListTest {
@@ -19,18 +20,5 @@ public class TaskListTest {
         assertEquals(testTaskList.getListSize(), 1);
         testTaskList.addTask(newTask);
         assertEquals(testTaskList.getListSize(), 2);
-    }
-
-    @Test
-    public void testDeleteTask_success() {
-        TaskList testTaskList = new TaskList();
-        Task newTask = new Task(null, null, null, null);
-        testTaskList.addTask(newTask);
-        testTaskList.addTask(newTask);
-        testTaskList.addTask(newTask);
-        testTaskList.deleteTask(2);
-        assertEquals(testTaskList.getListSize(), 2);
-        testTaskList.deleteTask(1);
-        assertEquals(testTaskList.getListSize(), 1);
     }
 }
