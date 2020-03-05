66f33ec9bcfd9ca7f007142b0c2d77f8624142bb
trishaangelica <trishavl2@gmail.com>
2020-03-05 17:09:06 +0800

    Add skeleton classes (Done together as a team)

---
+++ src/main/java/seedu/duke/data/ShoppingList.java
@@ -0,0 +1,16 @@
+package seedu.duke.data;
+
+import java.util.ArrayList;
+
+public class ShoppingList {
+    private static ArrayList<Item> items = new ArrayList<>();
+    private static double budget;
+    private static double cost;
+
+    public ShoppingList(){
+    }
+
+
+
+
+}
