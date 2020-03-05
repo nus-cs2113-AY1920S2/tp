9673b51622c895224587ff792257d3acae101279
jichngan <jcngan97@gmail.com>
2020-03-05 11:47:00 +0800

    Add TaskList tasks argument into abstract method execute.

--- src/main/java/command/Command.java
+++ src/main/java/command/Command.java
@@ -1,7 +1,9 @@
 package command;
 
+import seedu.duke.TaskList;
+
 public abstract class Command {
     public static final String COMMAND_NAME = null; // todo: override this
 
-    public abstract void execute(); // todo: take TaskList, Ui, Storage as parameters
+    public abstract void execute(TaskList tasks); // todo: take TaskList, Ui, Storage as parameters
 }
