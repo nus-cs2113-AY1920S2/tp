515706d72a003a39bedb43ff63e1739789490ae0
jichngan <jcngan97@gmail.com>
2020-03-05 18:01:50 +0800

    Fix coding style error.

--- src/main/java/command/ListCommand.java
+++ src/main/java/command/ListCommand.java
@@ -6,7 +6,7 @@ import seedu.duke.Ui;
 public class ListCommand extends Command {
 
     private final String listParam;
-    private final String LIST_TODAYCOMMAND = "today";
+    private final String TODAY_COMMAND = "today";
 
     public ListCommand(String listParam) {
         this.listParam = listParam;
@@ -14,7 +14,7 @@ public class ListCommand extends Command {
 
     @Override
     public void execute(TaskList taskList, Ui ui) {
-        if (listParam.equals(LIST_TODAYCOMMAND)) {
+        if (listParam.equals(TODAY_COMMAND)) {
             taskList.listTodayTasks();
         }
     }
