cf42996e8c4f7d360005198476b94906ccafe682
joelczk <joelchang97@gmail.com>
2020-03-07 20:18:38 +0800

    Added task type for Event and Assignment.

--- src/main/java/tasks/Assignment.java
+++ src/main/java/tasks/Assignment.java
@@ -3,7 +3,21 @@ package tasks;
 import java.time.LocalDateTime;
 
 public class Assignment extends Task {
+    protected String type;
+
+    /**
+     * Assignment object.
+     * @param name name of Assignment
+     * @param module module for Assignment
+     * @param deadline deadline of Assignment
+     * @param comments comments for Assignment
+     */
     public Assignment(String name, String module, LocalDateTime deadline, String comments) {
         super(name, module, deadline, comments);
+        this.type = "assignment";
+    }
+
+    public String getType() {
+        return type;
     }
 }
--- src/main/java/tasks/Event.java
+++ src/main/java/tasks/Event.java
@@ -3,7 +3,21 @@ package tasks;
 import java.time.LocalDateTime;
 
 public class Event extends Task {
+    protected String type;
+
+    /**
+     * Event object.
+     * @param name name of Event
+     * @param location location of Event
+     * @param dateTime date and time of Event
+     * @param comments comments for the Event
+     */
     public Event(String name, String location, LocalDateTime dateTime, String comments) {
         super(name, location, dateTime, comments);
+        this.type = "event";
+    }
+
+    public String getType() {
+        return type;
     }
 }
