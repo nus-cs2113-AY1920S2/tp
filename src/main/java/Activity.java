0f87c816143fa0b94b44a15c2f115a6745b24cc7
rdimaio <riccardodimaio11@gmail.com>
2020-03-08 17:31:41 +0800

    Add Storage and StorageHandler

--- src/main/java/Activity.java
+++ src/main/java/Activity.java
@@ -8,8 +8,34 @@ public class Activity {
         this.startTime = startTime;
     }
 
+    /**
+     * Constructor to create activity when endTime is previously known
+     * (i.e. when loading from storage)
+     *
+     * @param name Name of activity.
+     * @param startTime Time at which activity started.
+     * @param endTime Time at which activity ended.
+     */
+    public Activity(String name, String startTime, String endTime) {
+        this.name = name;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
     public String endActivity(String endTime) {
         this.endTime = endTime;
         return ("You have ended " + this.name + " . Great job !\n");
+    }
+
+    /**
+     * Converts the Activity object to data representation to be stored in a data file.
+     * File format:
+     * name, startTime, endTime
+     *
+     * @return String representing the Task object in comma-separated data format.
+     */
+    public String toData() {
+        String dataLine = (this.name + "," + this.startTime + "," + this.endTime);
+        return dataLine;
     }
 }
--- src/main/java/Jikan.java
+++ src/main/java/Jikan.java
@@ -1,6 +1,18 @@
+import storage.Storage;
+
 import java.util.Scanner;
 
+
 public class Jikan {
+    /** Constant file path of data file. */
+    private static final String DATA_FILE_PATH = "data/data.csv";
+
+    /** Storage object for data file. */
+    private static Storage storage;
+
+    /** Activity list to store current tasks in. */
+    private static ActivityList activityList;
+
     public static final String GREETING =
             "\n" +
                     "     _\n" +
@@ -16,13 +28,27 @@ public class Jikan {
                     "        /` /` \n" +
                     "Hello! I'm Jikan\n" + "What can I do for you?\n" +
                     "____________________________________________________________\n";
+
     /**
-     * Main entry-point for the java.duke.Duke application.
+     * Creates ActivityList and loads data from data file if the data file previously existed;
+     * otherwise, an empty task list is initialized.
+     */
+    private static void createActivityList() {
+        if (storage.loadFile()) {
+            activityList = new ActivityList(storage.dataFile);
+        } else {
+            activityList = new ActivityList();
+        }
+    }
+
+    /**
+     * Main entry-point for the Jikan application.
      */
     public static void main(String[] args) {
         System.out.println(GREETING);
         Scanner in = new Scanner(System.in);
-        ActivityList activityList = new ActivityList();
+        createActivityList();
+        storage = new Storage(DATA_FILE_PATH);
         Parser.parseUserCommands(in, activityList);
     }
 }
