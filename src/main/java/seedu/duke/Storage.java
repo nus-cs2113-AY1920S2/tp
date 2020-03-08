8c7bec8a3af003a9e07e3b25d98e55bc2ab527da
kezia <keziakew98@gmail.com>
2020-03-09 02:13:48 +0800

    comply with gradle checkstyle

--- src/main/java/seedu/duke/Storage.java
+++ src/main/java/seedu/duke/Storage.java
@@ -1,18 +1,22 @@
 package seedu.duke;
 
 import seedu.cards.Card;
-import seedu.cards.CardList;
 
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.ObjectInputStream;
+import java.io.FileInputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 
-public class Storage  {
+public class Storage {
 
     private static String dir = System.getProperty("user.dir");
     private static Path filepath = Paths.get(dir, "data", "cards.txt");
-    private static String filepathStr  = String.valueOf(filepath);;
+    private static String filepathStr  = String.valueOf(filepath);
     private static File saveFile = new File(filepathStr);
 
     /**
@@ -21,26 +25,23 @@ public class Storage  {
      */
     public static ArrayList<Card> loadCards() {
         ArrayList<Card> loadCards = new ArrayList<>();
-        if(!saveFile.exists()) {
+        if (!saveFile.exists()) {
             try {
                 // creates all sub dir if not exist
                 saveFile.getParentFile().mkdirs();
                 saveFile.createNewFile();
-            }
-            catch (IOException e){
+            } catch (IOException e) {
                 System.out.println("File creation error");
             }
-        }
-        else {
-            try{
+        } else {
+            try {
                 FileInputStream fileRead = new FileInputStream(saveFile);
                 ObjectInputStream objRead = new ObjectInputStream(fileRead);
 
                 loadCards = (ArrayList<Card>) objRead.readObject();
                 objRead.close();
-            }
-            catch (IOException | ClassNotFoundException e) {
-                System.out.println("\nLoad error" +e);
+            } catch (IOException | ClassNotFoundException e) {
+                System.out.println("\nLoad error");
             }
         }
         return loadCards;
@@ -51,16 +52,15 @@ public class Storage  {
      * @param currCards the current card list
      */
     public void saveCards(ArrayList<Card> currCards) {
-        try{
+        try {
             FileOutputStream fileWrite = new FileOutputStream(saveFile);
             ObjectOutputStream objWrite = new ObjectOutputStream(fileWrite);
 
             objWrite.writeObject(currCards);
             objWrite.flush();
             objWrite.close();
-        }
-        catch (IOException e) {
-            System.out.println("\nSave error " +e);
+        } catch (IOException e) {
+            System.out.println("\nSave error");
         }
     }
 }
