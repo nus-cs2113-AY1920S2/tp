2bb162e4c7b9b283101a1def503e85640e7ceded
Shannonwje <e0325213@u.nus.edu>
2020-03-08 18:36:30 +0800

    Edited to Coding Standard

--- src/main/java/seedu/duke/commands/MarkCommand.java
+++ src/main/java/seedu/duke/commands/MarkCommand.java
@@ -5,9 +5,9 @@ import seedu.duke.data.ShoppingList;
 
 public class MarkCommand extends Command {
     public static final String COMMAND_WORD = "MARK";
-    public static final String SUCCESS_DONE = "Yes! I've marked this item as bought: "
+    public static final String SUCCESS_MESSAGE = "Yes! I've marked this item as bought: "
                                                 + System.lineSeparator() + "%s";
-    public static final String FAIL_DONE = "Oh No! This item does not exist in the list";
+    public static final String FAIL_MESSAGE = "Oh No! This item does not exist in the list";
     private int indexOfItem;
 
     public MarkCommand(int index) {
@@ -18,9 +18,9 @@ public class MarkCommand extends Command {
     public CommandResult execute() {
         try {
             Item markItem = ShoppingList.markAsBought(indexOfItem);
-            return new CommandResult(String.format(SUCCESS_DONE, markItem));
+            return new CommandResult(String.format(SUCCESS_MESSAGE, markItem));
         } catch (ItemNotFoundException Inf) {
-            return new CommandResult(FAIL_DONE);
+            return new CommandResult(FAIL_MESSAGE);
         }
     }
 }
--- src/main/java/seedu/duke/commands/UnmarkCommand.java
+++ src/main/java/seedu/duke/commands/UnmarkCommand.java
@@ -5,9 +5,9 @@ import seedu.duke.data.ShoppingList;
 
 public class UnmarkCommand extends Command {
     public static final String COMMAND_WORD = "UNMARK";
-    public static final String SUCCESS_DONE = "Yes! I've unmarked this item as bought: "
+    public static final String SUCCESS_MESSAGE = "Yes! I've unmarked this item as bought: "
                                              + System.lineSeparator() + "%s";
-    public static final String FAIL_DONE = "Oh No! This item does not exist in the list";
+    public static final String FAIL_MESSAGE = "Oh No! This item does not exist in the list";
     private int indexOfItem;
 
     public UnmarkCommand(int index) {
@@ -18,9 +18,9 @@ public class UnmarkCommand extends Command {
     public CommandResult execute() {
         try {
             Item unmarkItem = ShoppingList.unmarkAsBought(indexOfItem);
-            return new CommandResult(String.format(SUCCESS_DONE, unmarkItem));
+            return new CommandResult(String.format(SUCCESS_MESSAGE, unmarkItem));
         } catch (ItemNotFoundException Inf) {
-            return new CommandResult(FAIL_DONE);
+            return new CommandResult(FAIL_MESSAGE);
         }
     }
 }
