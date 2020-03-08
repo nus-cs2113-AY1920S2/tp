45a9506f6891a91951d1e7190141cc30c956279c
A11riseforme <ycl0217@hotmail.com>
2020-03-09 02:28:34 +0800

    add basic structure

---
+++ src/main/java/seedu/nuke/command/EnrolModuleCommand.java
@@ -0,0 +1,14 @@
+package seedu.nuke.command;
+
+import seedu.nuke.ui.Ui;
+import seedu.nuke.util.Storage;
+
+public class EnrolModuleCommand {
+    public boolean isExit() {
+        return false;
+    }
+
+    public void execute(Module module, int taskId, Ui ui, Storage storage) {
+
+    }
+}
---
+++ src/main/java/seedu/nuke/util/Storage.java
@@ -0,0 +1,9 @@
+package seedu.nuke.util;
+
+public class Storage {
+    private String dataFileName;
+
+    public Storage(String dataFileName) {
+        this.dataFileName = dataFileName;
+    }
+}
