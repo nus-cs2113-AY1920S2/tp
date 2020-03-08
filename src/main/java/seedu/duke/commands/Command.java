fd03294b2d7151de3b0698e83c28f4456258067c
trishaangelica <trishavl2@gmail.com>
2020-03-08 18:38:27 +0800

    Removed unncessary colons

--- src/main/java/seedu/duke/commands/Command.java
+++ src/main/java/seedu/duke/commands/Command.java
@@ -2,8 +2,6 @@ package seedu.duke.commands;
 
 import seedu.duke.data.ShoppingList;
 
-import java.util.List;
-
 public class Command {
     /**
      * Executes the command and returns the result.
@@ -12,7 +10,7 @@ public class Command {
 
     public CommandResult execute() {
         throw new UnsupportedOperationException("This method is to be implemented by child classes");
-    };
+    }
 
     /**
      * Supplies the data the command will operate on.
