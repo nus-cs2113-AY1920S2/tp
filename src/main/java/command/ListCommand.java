1d56bbece561b2eb0a423697521e2e9826bb14c1
Joseph Lim <limweijie@live.com.sg>
2020-03-10 00:28:08 +0800

    Add Command classes.

---
+++ src/main/java/command/ListCommand.java
@@ -0,0 +1,24 @@
+package command;
+
+/**
+ * This class focuses on the 'list' functionality of the application.
+ * 
+ */
+public class ListCommand extends Command {
+    
+    private final String category;
+    
+    public ListCommand(String category) {
+        this.category = category;
+    }
+    
+    @Override
+    public CommandResult execute() {
+        switch (category) {
+        case ("stock") :
+            this.stock.listIngredient();
+        }
+        
+        return new CommandResult("All ingredients listed successfully!");
+    }
+}
