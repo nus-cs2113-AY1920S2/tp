479e3beb8f49080c35032692c5bdafdb5fa224ef
BenjaminChan <42601850+benchan911@users.noreply.github.com>
2020-03-09 17:10:25 +0800

    [Update] JavaDoc

--- src/main/java/seedu/command/CommandInterpreter.java
+++ src/main/java/seedu/command/CommandInterpreter.java
@@ -6,6 +6,13 @@ import static seedu.parser.Parser.getCommandWord;
 
 public class CommandInterpreter {
 
+    /**
+     * Execute the command from userInput.
+     *
+     * @param userInput The userInput from the Ui.
+     * @return The command object.
+     * @throws DukeException If the command is undefined.
+     */
     public static Command executeCommand(String userInput) throws DukeException {
         String commandWord = getCommandWord(userInput);
         switch (commandWord) {
