940daaeb568b782af1ed41d2c7544ca7a300ee11
bennychanya <40769130+bennychanya@users.noreply.github.com>
2020-03-10 16:08:35 +0800

    Update ExitCommand.java

--- src/main/java/seedu/duke/command/ExitCommand.java
+++ src/main/java/seedu/duke/command/ExitCommand.java
@@ -2,7 +2,7 @@ package seedu.duke.command;
 
 public class ExitCommand extends Command {
 
-    public static final String COMMAND_WORD = "bye";
+    public static final String COMMAND_WORD = "bye!";
 
     public boolean isExit() {
         return true;
