5dce5610ec26b0378cbe404d130b3f9c51e62323
siuhian <42374522+siuhian@users.noreply.github.com>
2020-03-10 23:56:06 +0800

    Add a Ui class and refactor some code

--- src/main/java/jikan/Jikan.java
+++ src/main/java/jikan/Jikan.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 import java.util.Scanner;
 
 /**
- * Represents the jikan.Jikan time tracker.
+ * Represents the Jikan time tracker.
  */
 public class Jikan {
     /** Constant file path of data file. */
@@ -17,26 +17,19 @@ public class Jikan {
     /** Storage object for data file. */
     private static Storage storage;
 
-    /** jikan.activity.Activity list to store current tasks in. */
+    /** Activity list to store current tasks in. */
     private static ActivityList activityList;
 
-    public static final String GREETING = "___________________________________________\n"
-            + "   0101 01010101 01   01  .010.  101   01 \n"
-            + "   `10'   `10'   10 ,10' 10' `10 101o  01 \n"
-            + "    01     01    01,01   01   01 0101o 10 \n"
-            + "    10     10    10`10   10ooo10 10 11o01 \n"
-            + "01. 01    .01.   01 `01. 01   01 01  1010 \n"
-            + "010101  01010101 10   10 10   10 10   110 \n"
-            + "___________________________________________\n"
-            + " Hello! I'm jikan.Jikan\n"
-            + " What can I do for you today?\n"
-            + "-------------------------------------------\n";
+    /** Ui to handle printing. */
+    private static Ui ui = new Ui();
+
+    private static Parser parser = new Parser();
     
     /**
-     * Creates jikan.activity.ActivityList and loads data from data file if the data file previously existed;
+     * Creates ActivityList and loads data from data file if the data file previously existed;
      * otherwise, an empty task list is initialized.
      */
-    private static void createActivityList() {
+    public static void createActivityList() {
         try {
             if (storage.loadFile()) {
                 activityList = new ActivityList(storage);
@@ -50,13 +43,13 @@ public class Jikan {
     }
   
     /**
-     * Main entry-point for the jikan.Jikan application.
+     * Main entry-point for the Jikan application.
      */
     public static void main(String[] args) {
-        System.out.println(GREETING);
+        ui.printGreeting();
         Scanner in = new Scanner(System.in);
         storage = new Storage(DATA_FILE_PATH);
         createActivityList();
-        Parser.parseUserCommands(in, activityList);
+        parser.parseUserCommands(in, activityList);
     }
 }
---
+++ src/main/java/jikan/Ui.java
@@ -0,0 +1,63 @@
+package jikan;
+
+import jikan.activity.ActivityList;
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+public class Ui {
+    public static final String GREETING = "___________________________________________\n"
+            + "   0101 01010101 01   01  .010.  101   01 \n"
+            + "   `10'   `10'   10 ,10' 10' `10 101o  01 \n"
+            + "    01     01    01,01   01   01 0101o 10 \n"
+            + "    10     10    10`10   10ooo10 10 11o01 \n"
+            + "01. 01    .01.   01 `01. 01   01 01  1010 \n"
+            + "010101  01010101 10   10 10   10 10   110 \n"
+            + "___________________________________________\n"
+            + " Hello! I'm Jikan\n"
+            + " What can I do for you today?";
+
+    public static final String DIVIDER = "-------------------------------------------";
+
+    /** Prints the logo and greeting so users know the app is working. */
+    public void printGreeting() {
+        System.out.println(GREETING);
+        System.out.println(DIVIDER);
+    }
+
+    /** Prints exit message and exits the app. */
+    public void exitFromApp() {
+        System.out.println(DIVIDER);
+        System.out.println(" Bye! See you again.");
+        System.out.println(DIVIDER);
+        System.exit(0);
+    }
+
+    /** Prints divider between user input and app feedback. */
+    public void printDivider(String line) {
+        System.out.println(DIVIDER);
+        System.out.println(line);
+        System.out.println(DIVIDER);
+    }
+
+    /** Prints all the activities in the list. */
+    public void printList(ActivityList activityList) {
+        System.out.println(DIVIDER);
+        System.out.println("Your completed activities:");
+        for (int i = 0; i < activityList.getSize(); i++) {
+            long durationInNanos = (activityList.get(i).duration).toNanos();
+            String duration = String.format("%02d:%02d:%02d",
+                    TimeUnit.NANOSECONDS.toHours(durationInNanos),
+                    TimeUnit.NANOSECONDS.toMinutes(durationInNanos)
+                            - TimeUnit.HOURS.toMinutes(TimeUnit.NANOSECONDS.toHours(durationInNanos)),
+                    TimeUnit.NANOSECONDS.toSeconds(durationInNanos)
+                            - TimeUnit.MINUTES.toSeconds(TimeUnit.NANOSECONDS.toMinutes(durationInNanos)));
+            System.out.print(i + 1 + ". " + activityList.get(i).name + " " + duration);
+            if (activityList.get(i).tags != null) {
+                System.out.println(" " + Arrays.toString(activityList.get(i).tags));
+            } else {
+                System.out.print("\n");
+            }
+        }
+        System.out.println(DIVIDER);
+    }
+}
