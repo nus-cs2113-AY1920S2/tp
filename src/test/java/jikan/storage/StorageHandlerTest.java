b2ef6f01115c46daefbb19b89e7a48b6d810ac56
rdimaio <riccardodimaio11@gmail.com>
2020-03-10 18:30:17 +0800

    Fixed error in tests

--- src/test/java/jikan/storage/StorageHandlerTest.java
+++ src/test/java/jikan/storage/StorageHandlerTest.java
@@ -8,9 +8,6 @@ import java.util.List;
 import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class StorageHandlerTest {
 
