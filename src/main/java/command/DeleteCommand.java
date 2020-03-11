195648f1ca1cd055d245ecd310c8d53996f187f8
lwxymere <chaoscold@hotmail.com>
2020-03-11 16:42:34 +0800

    Modify error condition

--- src/main/java/command/DeleteCommand.java
+++ src/main/java/command/DeleteCommand.java
@@ -1,7 +1,6 @@
 package command;
 
 import common.Messages;
-import exceptions.DukeException;
 import seedu.duke.TaskList;
 import seedu.duke.Ui;
 import tasks.Task;
@@ -19,12 +18,14 @@ public class DeleteCommand extends Command {
 
     @Override
     public CommandResult execute(TaskList taskList, Ui ui) {
+        if (taskList.getListSize() == 0) {
+            return new CommandResult(Messages.NO_TASKS_MSG);
+        }
         try {
             Task taskToBeDeleted = taskList.getTask(deleteIndex);
             taskList.deleteTask(deleteIndex);
-            return new CommandResult(String.format(Messages.DELETE_SUCCESS_MESSAGE, deleteIndex + 1,
-                    taskToBeDeleted.getName()));
-        } catch (IndexOutOfBoundsException | NullPointerException e) {
+            return new CommandResult(String.format(Messages.DELETE_SUCCESS_MESSAGE, taskToBeDeleted.getName()));
+        } catch (IndexOutOfBoundsException e) {
             return new CommandResult(String.format(Messages.INVALID_ID_ERROR, getRangeOfValidIndex(taskList)));
         }
     }
