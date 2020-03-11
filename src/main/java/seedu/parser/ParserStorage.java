01277b643607379f09656bbca11e3a9e6e39ba06
BenjaminChan <42601850+benchan911@users.noreply.github.com>
2020-03-11 22:12:28 +0800

    Remove DateTime to avoid merge Conflict

--- src/main/java/seedu/parser/ParserStorage.java
+++ src/main/java/seedu/parser/ParserStorage.java
@@ -6,35 +6,35 @@ import seedu.exception.DukeException;
 public class ParserStorage {
 
     /**
-     * Parses a task from String format back to task.
+     * Parses an event from String format back to task.
      *
-     * @param line description of the task.
+     * @param line description of the event.
      * @return The corresponding task object.
      * @throws DukeException If the line is corrupted.
      */
 
-    public static Event createTaskFromStorage(String line) throws DukeException {
-        String[] taskParts = line.split("\\|");
-        try {
-            String name = taskParts[0].strip();
-            String datetime = taskParts[1].strip();
-            String venue = taskParts[2].strip();
-            Event event = new Event(name,datetime,venue);
-            return event;
-        } catch (Exception e) {
-            throw new DukeException("CORRUPTED_TASK");
-        }
-    }
+//    public static Event createTaskFromStorage(String line) throws DukeException {
+//        String[] taskParts = line.split("\\|");
+//        try {
+//            String name = taskParts[0].strip();
+//            String datetime = taskParts[1].strip();
+//            String venue = taskParts[2].strip();
+//            Event event = new Event(name,datetime,venue);
+//            return event;
+//        } catch (Exception e) {
+//            throw new DukeException("CORRUPTED_TASK");
+//        }
+//    }
 
     /**
-     * Parses a task from task to String format.
+     * Parses an event from events to String format.
      *
-     * @param event The task.
+     * @param event The event.
      * @return The corresponding String format of the task object.
      */
 
     public static String toStorageString(Event event) {
-        return "D | " + event.getName() + " | " + event.getDatetime() + " | " + event.getVenue();
+        return event.getName() + " | " + event.getDatetime() + " | " + event.getVenue();
     }
 
 }
