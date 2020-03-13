b1a8898d21b38916f36bdf543867456bd51544a8
Joseph Lim <limweijie@live.com.sg>
2020-03-13 16:08:54 +0800

    Fix import assertTrue statement.

--- src/test/java/stock/StockTest.java
+++ src/test/java/stock/StockTest.java
@@ -1,8 +1,8 @@
 package stock;
 
-import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
