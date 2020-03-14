bf9f4737b83775e8c09d3d26fb13c55232f8b014
DeetoMok <darylmok@u.nus.edu>
2020-03-14 21:43:07 +0800

    Add JUnit of SelectedModule class

---
+++ src/test/java/seedu/duke/module/SelectedModuleTest.java
@@ -0,0 +1,16 @@
+package seedu.duke.module;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SelectedModuleTest{
+
+    @Test
+    public void testMarkAsDone() {
+        Module module = new Module("id", "CS1010", "19/20 Sem2");
+        assertEquals("[✗]", module.getIcon());
+        module.setAsDone();
+        assertEquals("[✓]", module.getIcon());
+    }
+}
