58b682a918c726c156f5fdb2111c48500447a03e
DeetoMok <darylmok@u.nus.edu>
2020-03-14 21:48:22 +0800

    reduced checkStyle error

--- src/test/java/seedu/duke/module/SelectedModuleTest.java
+++ src/test/java/seedu/duke/module/SelectedModuleTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class SelectedModuleTest{
+public class SelectedModuleTest {
 
     @Test
     public void testMarkAsDone() {
