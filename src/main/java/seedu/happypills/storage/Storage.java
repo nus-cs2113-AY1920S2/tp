72dee677997973f909625b245ee4526bc4a3109f
Janicetyy <janice.tohy2@gmail.com>
2020-03-16 17:03:58 +0800

    added new storage functions

---
+++ src/main/java/seedu/happypills/storage/Storage.java
@@ -0,0 +1,65 @@
+package seedu.happypills.storage;
+
+import seedu.happypills.data.Patient;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+/**
+ * A class that manages read and write operations to file
+ */
+public class Storage {
+    /**
+     * Save individual patient data as strings to file. Creates file if it does not exist.
+     *
+     * @param filePath location of file to save to, requires directory/file
+     * @param patientDataString  patient data as string to be saved to file
+     * @throws IOException if unable to save to file, possibly due to interruptions
+     */
+    public static void addToFile(String filePath, String patientDataString) throws IOException {
+        File d = new File(filePath.substring(0, filePath.lastIndexOf('/')));
+        if (!d.exists()) {
+            d.mkdir();
+        }
+
+        File f = new File(filePath);
+        if (!f.exists()) {
+            f.createNewFile();
+        }
+
+        FileWriter fw = new FileWriter(filePath);
+        fw.write(patientDataString);
+        fw.close();
+    }
+
+    /**
+     * Read in file as strings
+     *
+     * @param filePath location of file to read from
+     * @return <code>ArrayList</code> of each line read from the file
+     * @throws FileNotFoundException if the file specified by <code>filePath</code> does not exist
+     */
+    public static ArrayList<Patient> loadFromFile(String filePath) throws FileNotFoundException {
+        File f = new File(filePath);
+        Scanner s = new Scanner(f);
+        ArrayList<Patient> storedPatients = new ArrayList<>();
+
+        while (s.hasNext()) {
+            String stringInput = s.nextLine();
+            parseFileContent(stringInput, storedPatients);
+        }
+
+        return storedPatients;
+    }
+
+    private static void parseFileContent(String savedString, ArrayList<Patient> storedPatients) {
+        String[] dataString = savedString.split("[|]", 7);
+        Patient tempPatient = new Patient(dataString[0], dataString[1], Integer.parseInt(dataString[2]), dataString[3], dataString[4], dataString[5], dataString[6]);
+        storedPatients.add(tempPatient);
+    }
+}
