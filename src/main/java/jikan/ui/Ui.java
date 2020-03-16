0918f8da6baf7dcf099c7c2381c685b4819427d7
rdimaio <riccardodimaio11@gmail.com>
2020-03-16 18:28:44 +0800

    Change logo to plain characters

--- src/main/java/jikan/ui/Ui.java
+++ src/main/java/jikan/ui/Ui.java
@@ -5,16 +5,7 @@ import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
 public class Ui {
-    public static final String GREETING = "___________________________________________\n"
-            + "   0101 01010101 01   01  .010.  101   01 \n"
-            + "   `10'   `10'   10 ,10' 10' `10 101o  01 \n"
-            + "    01     01    01,01   01   01 0101o 10 \n"
-            + "    10     10    10`10   10ooo10 10 11o01 \n"
-            + "01. 01    .01.   01 `01. 01   01 01  1010 \n"
-            + "010101  01010101 10   10 10   10 10   110 \n"
-            + "___________________________________________\n"
-            + " Hello! I'm Jikan\n"
-            + " What can I do for you today?";
+    public static final String GREETING = "Jikan";
 
     public static final String DIVIDER = "-------------------------------------------";
 
