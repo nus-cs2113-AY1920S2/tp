4ac708db1b2e18f7ab1fac34bc2b1b1211f8a731
wangqinNick <347656409nick@gmail.com>
2020-03-16 12:52:55 +0800

    temporary commit

--- src/main/java/seedu/nuke/command/UndoCommand.java
+++ src/main/java/seedu/nuke/command/UndoCommand.java
@@ -3,6 +3,8 @@ package seedu.nuke.command;
 import seedu.nuke.data.ScreenShot;
 import seedu.nuke.data.ScreenShotManager;
 
+import static seedu.nuke.util.Message.MESSAGE_UNDO_SUCCESSFUL;
+
 public class UndoCommand extends Command {
     public static final String COMMAND_WORD = "un";
     public static final String MESSAGE_USAGE = COMMAND_WORD;
@@ -16,6 +18,6 @@ public class UndoCommand extends Command {
     public CommandResult execute() {
         //change to the screen one step before
         targetScreenShot = screenShotManager.unDo();
-        return null;
+        return new CommandResult(MESSAGE_UNDO_SUCCESSFUL);
     }
 }
