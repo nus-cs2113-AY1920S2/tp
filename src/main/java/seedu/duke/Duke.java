722702b5d57c6be6076cb5befc5e18f2da0b25a1
Tshin Qi Ren <e0309556@u.nus.edu>
2020-03-16 16:13:58 +0800

    Remove assertion added for test

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -42,7 +42,6 @@ public class Duke {
      * Main entry-point for the java.duke.Duke application.
      */
     public static void main(String[] args) {
-        assert false : "dummy assertion set to fail";
         new Duke().run();
     }
 }
