d6e10d61ee7b9b7973f4a63f31e388142971be21
Jeremy Cheung <z5142254@ad.unsw.edu.au>
2020-03-18 02:48:18 +1100

    add subject and subjectList

---
+++ src/test/java/seedu/subjects/SubjectTest.java
@@ -0,0 +1,26 @@
+package seedu.subjects;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import seedu.Subjects.Subject;
+import seedu.cards.Card;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SubjectTest {
+    private Subject subject1;
+    private Subject subject2;
+
+    @BeforeEach
+    public void setUp() {
+        subject1 = new Subject("Test 1");
+        subject2 = new Subject("Test 2");
+    }
+
+    @Test
+    public void getSubject() {
+        assertTrue(subject1.getSubject().equals("Test 1"));
+        assertTrue(subject2.getSubject().equals("Test 2"));
+    }
+
+}
