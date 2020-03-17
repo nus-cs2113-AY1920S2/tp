c9f8c2ba4d2b83a0c540cd74dfc5cad6a9c0083e
iceclementi <iceclementi@gmail.com>
2020-03-18 04:56:09 +0800

    Add a utility class List Creator to create list tables from Array Lists

---
+++ src/main/java/seedu/nuke/util/ListCreator.java
@@ -0,0 +1,271 @@
+package seedu.nuke.util;
+
+import seedu.nuke.directory.Category;
+import seedu.nuke.directory.Module;
+import seedu.nuke.directory.Task;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+/**
+ * A utility class to create a sorted list table from an Array List.
+ */
+public class ListCreator {
+    private static final String LIST_DIVIDER = String.format("%s%s%s\n", "+", "-".repeat(78), "+");
+    private static final String SEPARATOR = "|";
+
+    /**
+     * Sorts modules in a list by their module codes.
+     *
+     * @param toSort
+     *  The list of modules to be sorted
+     */
+   public static void sortModuleList(ArrayList<Module> toSort) {
+        Comparator<Module> sortByModule =
+                Comparator.comparing(Module::getModuleCode);
+
+        toSort.sort(sortByModule);
+    }
+
+    /**
+     * Sorts categories in a list by their names.
+     *
+     * @param toSort
+     *  The list of categories to be sorted
+     */
+    public static void sortCategoryList(ArrayList<Category> toSort) {
+        Comparator<Category> sortByModule =
+                Comparator.comparing(category -> ((Module)category.getParent()).getModuleCode());
+        Comparator<Category> sortByCategory =
+                Comparator.comparing(Category::getCategoryName);
+
+        toSort.sort(sortByModule.thenComparing(sortByCategory));
+    }
+
+
+    /**
+     * Sorts tasks in a list by their description.
+     *
+     * @param toSort
+     *  The list of tasks to be sorted
+     */
+    public static void sortTaskList(ArrayList<Task> toSort) {
+        Comparator<Task> sortByModule =
+                Comparator.comparing(task -> ((Module)task.getParent().getParent()).getModuleCode());
+        Comparator<Task> sortByCategory =
+                Comparator.comparing(task -> ((Category)task.getParent()).getCategoryName());
+        Comparator<Task> sortByTask =
+                Comparator.comparing(Task::getDescription);
+
+        toSort.sort(sortByModule.thenComparing(sortByCategory).thenComparing(sortByTask));
+    }
+
+    /**
+     * Creates a sorted module list table from a list of modules
+     *
+     * @param moduleList
+     *  The list of modules to be converted into a table
+     * @return
+     *  The sorted module list table
+     */
+    public static String createModuleListTable(ArrayList<Module> moduleList) {
+        sortModuleList(moduleList);
+        StringBuilder moduleListTable = new StringBuilder();
+
+        moduleListTable.append(createModuleListTableHeader());
+
+        int id = 1;
+        for (Module module : moduleList) {
+            String moduleCode = module.getModuleCode();
+            String moduleTitle = (module.getTitle()!=null) ? module.getTitle() : "-NIL-";
+
+            moduleListTable.append(String.format("%s%s%s%s%s\n",
+                    centraliseText(fitText(Integer.toString(id++), 4), 4), SEPARATOR,
+                    centraliseText(fitText(moduleCode, 16), 16), SEPARATOR,
+                    centraliseText(fitText(moduleTitle, 58), 58)
+            ));
+        }
+
+        moduleListTable.append(LIST_DIVIDER);
+        moduleListTable.append(String.format("Total modules: %d\n", moduleList.size()));
+        moduleListTable.append(LIST_DIVIDER);
+
+        return moduleListTable.toString();
+    }
+
+    /**
+     * Creates the header of the module list table
+     *
+     * @return
+     *  The header of the module list table
+     */
+    private static String createModuleListTableHeader() {
+        StringBuilder header = new StringBuilder();
+
+        header.append(LIST_DIVIDER);
+        header.append(String.format("%s%s%s%s%s\n",
+                centraliseText("ID", 4), SEPARATOR,
+                centraliseText("MODULE CODE", 16), SEPARATOR,
+                centraliseText("MODULE TITLE", 58)
+        ));
+        header.append(LIST_DIVIDER);
+
+        return header.toString();
+    }
+
+    /**
+     * Creates a sorted category list table from a list of categories
+     *
+     * @param categoryList
+     *  The list of categories to be converted into a table
+     * @return
+     *  The sorted category list table
+     */
+    public static String createCategoryListTable(ArrayList<Category> categoryList) {
+        sortCategoryList(categoryList);
+        StringBuilder categoryListTable = new StringBuilder();
+
+        categoryListTable.append(createCategoryListTableHeader());
+
+        int id = 1;
+        for (Category category : categoryList) {
+            String moduleCode = ((Module)category.getParent()).getModuleCode();
+            String categoryName = category.getCategoryName();
+
+            categoryListTable.append(String.format("%s%s%s%s%s\n",
+                    centraliseText(fitText(Integer.toString(id++), 4), 4), SEPARATOR,
+                    centraliseText(fitText(moduleCode, 16), 16), SEPARATOR,
+                    centraliseText(fitText(categoryName, 58), 58)
+            ));
+        }
+
+        categoryListTable.append(LIST_DIVIDER);
+        categoryListTable.append(String.format("Total categories: %d\n", categoryList.size()));
+        categoryListTable.append(LIST_DIVIDER);
+
+        return categoryListTable.toString();
+    }
+
+    /**
+     * Creates the header of the category list table
+     *
+     * @return
+     *  The header of the category list table
+     */
+    private static String createCategoryListTableHeader() {
+        StringBuilder header = new StringBuilder();
+
+        header.append(LIST_DIVIDER);
+        header.append(String.format("%s%s%s%s%s\n",
+                centraliseText("ID", 4), SEPARATOR,
+                centraliseText("MODULE", 16), SEPARATOR,
+                centraliseText("CATEGORY", 58)
+        ));
+        header.append(LIST_DIVIDER);
+
+        return header.toString();
+    }
+
+    /**
+     * Creates a sorted task list table from a list of tasks
+     *
+     * @param taskList
+     *  The list of tasks to be converted into a table
+     * @return
+     *  The sorted task list table
+     */
+    public static String createTaskListTable(ArrayList<Task> taskList) {
+        sortTaskList(taskList);
+        StringBuilder taskListTable = new StringBuilder();
+
+        taskListTable.append(createTaskListTableHeader());
+
+        int id = 1;
+        for (Task task : taskList) {
+            String moduleCode = ((Module)task.getParent().getParent()).getModuleCode();
+            String categoryName = ((Category)task.getParent()).getCategoryName();
+            String taskDescription = task.getDescription();
+            String deadline = (task.getDeadline() != null) ? task.getDeadline().toString() : "-NIL-";
+
+            taskListTable.append(String.format("%s%s%s%s%s%s%s%s%s\n",
+                    centraliseText(fitText(Integer.toString(id++), 4), 4), SEPARATOR,
+                    centraliseText(fitText(moduleCode, 10), 10), SEPARATOR,
+                    centraliseText(fitText(categoryName, 18), 18), SEPARATOR,
+                    centraliseText(fitText(taskDescription, 24), 24), SEPARATOR,
+                    centraliseText(fitText(deadline, 24), 24)
+            ));
+        }
+
+        taskListTable.append(LIST_DIVIDER);
+        taskListTable.append(String.format("Total tasks: %d\n", taskList.size()));
+        taskListTable.append(LIST_DIVIDER);
+
+        return taskListTable.toString();
+    }
+
+    /**
+     * Creates the header of the task list table
+     *
+     * @return
+     *  The header of the task list table
+     */
+    private static String createTaskListTableHeader() {
+        StringBuilder header = new StringBuilder();
+
+        header.append(LIST_DIVIDER);
+        header.append(String.format("%s%s%s%s%s%s%s%s%s\n",
+                centraliseText("ID", 4), SEPARATOR,
+                centraliseText("MODULE", 10), SEPARATOR,
+                centraliseText("CATEGORY", 18), SEPARATOR,
+                centraliseText("TASK", 24), SEPARATOR,
+                centraliseText("DEADLINE", 20)
+        ));
+        header.append(LIST_DIVIDER);
+
+        return header.toString();
+    }
+
+    /**
+     * Adds the specified number of spaces.
+     *
+     * @param numberOfSpaces
+     *  The number of spaces to be added
+     * @return
+     *  A string containing the specified number of spaces
+     */
+    private static String addSpaces(int numberOfSpaces) {
+        return " ".repeat(numberOfSpaces);
+    }
+
+    /**
+     * Centralises the given text in a segment of given length by padding with spaces before and after the text.
+     *
+     * @param text
+     *  The text to be centralised
+     * @param segmentLength
+     *  The length of the segment in which the text is centralised
+     * @return
+     *  The centralised text
+     */
+    private static String centraliseText(String text, int segmentLength) {
+        int numberOfLeftSpaces = (segmentLength - text.length()) / 2;
+        int numberOfRightSpaces = (segmentLength - text.length() + 1) / 2;
+        return addSpaces(numberOfLeftSpaces) + text + addSpaces(numberOfRightSpaces);
+    }
+
+    /**
+     * Fits the given text into a segment of given length, truncating the text with ellipses if the text exceeds
+     * the segment length.
+     *
+     * @param text
+     *  The text to be fitted
+     * @param segmentLength
+     *  The length of the segment in which the text is fitted
+     * @return
+     *  The fitted text
+     */
+    private static String fitText(String text, int segmentLength) {
+        final String ELLIPSES = "...";
+        return (text.length() > segmentLength) ? (text.substring(0 , segmentLength-3) + ELLIPSES) : text;
+    }
+}
