27a28bacdc2501f003c46064ec8187dcff55722b
bennychanya <bennychanyongan@live.com.sg>
2020-03-20 21:27:55 +0800

    Changed import for parser class to pass build test

--- src/main/java/seedu/duke/parser/Parser.java
+++ src/main/java/seedu/duke/parser/Parser.java
@@ -1,6 +1,14 @@
 package seedu.duke.parser;
 
-import seedu.duke.command.*;
+import seedu.duke.command.Command;
+import seedu.duke.command.CalculateCapCommand;
+import seedu.duke.command.ExitCommand;
+import seedu.duke.command.AddToDataCommand;
+import seedu.duke.command.HelpingCommand;
+import seedu.duke.command.AddToSemCommand;
+import seedu.duke.command.AddCommand;
+import seedu.duke.command.ViewCommand;
+import seedu.duke.command.MarkAsDoneCommand;
 import seedu.duke.data.Person;
 import seedu.duke.exception.InputException;
 import seedu.duke.module.Grading;
