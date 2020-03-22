c962ec8172eb4daa0c5bbc9864bcf3df175198e5
Joseph Lim <limweijie@live.com.sg>
2020-03-22 17:30:18 +0800

    Add restaurant daily report logo in welcome message.

--- src/main/java/ui/Ui.java
+++ src/main/java/ui/Ui.java
@@ -12,13 +12,19 @@ public class Ui {
     
     private final Scanner in;
     private final PrintStream out;
-    
+    private final String ls = System.lineSeparator();
+                                          
     public Ui() {
         this(System.in, System.out);
     }
     
     public void showWelcomeMessage() {
-        System.out.println("WELCOME!!!");
+        System.out.println("______  ______  _____ ______    ___  __  __ ______        __  _______  ______");
+        System.out.println("||    ||||      \\       ||     / ||  ||  || ||    |      / || ||    ||   ||");       
+        System.out.println("||    ||||       \\      ||    /  ||  ||  || ||    |     /  || ||    ||   ||");
+        System.out.println("||__ /  ||___     \\___  ||   /___||  ||  || ||__ /     /___|| ||    ||   ||");
+        System.out.println("||   \\\\ ||           \\  ||  /    ||  ||  || ||   \\\\   /    || ||    ||   ||");
+        System.out.println("||    \\\\||____   _____\\ || /     ||   \\__/  ||    \\\\ /     Daily Report v2.0");       
     }
     
     public Ui(InputStream in, PrintStream out) {
