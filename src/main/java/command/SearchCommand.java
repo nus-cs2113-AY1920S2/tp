883d810f2a159d8e53865e2a50fb937aac30d3b9
joelczk <joelchang97@gmail.com>
2020-03-22 10:03:21 +0800

    Changed display message from printing to string format. Changed tests for search accordingly.

--- src/main/java/command/SearchCommand.java
+++ src/main/java/command/SearchCommand.java
@@ -1,12 +1,9 @@
 package command;
 
 import common.Messages;
-import exceptions.AtasException;
 import seedu.atas.TaskList;
 import seedu.atas.Ui;
 import tasks.Task;
-
-import exceptions.AtasException;
 
 import java.util.ArrayList;
 
@@ -74,20 +71,35 @@ public class SearchCommand extends Command {
     }
 
     /**
-     * prints out the search query.
-     * @param results ArrayList containing the results of the search query
+     * Returns list of search queries.
+     * @param results ArrayList containing the results of search query
+     * @return list of search queries
      */
-    private void printResultsList(ArrayList<Task> results) {
+    private String searchList(ArrayList<Task> results) {
+        assert results.size() > 0;
+        int position = 1;
+        StringBuilder searchString = new StringBuilder();
+        searchString.append(Messages.SEARCH_SUCCESS_MESSAGE);
+        searchString.append(System.lineSeparator());
+        for (Task task: results) {
+            searchString.append(String.format("%3d.%s", position, task.toString()));
+            searchString.append(System.lineSeparator());
+            position++;
+        }
+        return searchString.toString();
+    }
+
+    /**
+     * Returns String format of search queries.
+     * @param results ArrayList of the results of search queries.
+     * @return String format of search queries
+     */
+    private String resultsList(ArrayList<Task> results) {
         if (results.size() == 0) {
-            System.out.println(String.format(Messages.EMPTY_SEARCH_RESULTS_ERROR));
+            return (Messages.EMPTY_SEARCH_RESULTS_ERROR);
         } else {
-            assert results.size() > 0;
-            int position = 1;
-            System.out.println("Here are the search results:");
-            for (Task task: results) {
-                System.out.println(position + ". " + task.toString());
-                position++;
-            }
+            String resultsString = searchList(results);
+            return (String.format(resultsString));
         }
     }
 
@@ -99,16 +111,13 @@ public class SearchCommand extends Command {
         switch (taskType) {
         case allTasks:
             ArrayList<Task> results = getSearchQueryAllTasks(taskList);
-            printResultsList(results);
-            return new CommandResult(String.format(Messages.SEARCH_SUCCESS_MESSAGE, results.size()));
+            return new CommandResult(resultsList(results));
         case eventTasks:
             ArrayList<Task> eventResults = getSearchQueryEvents(taskList);
-            printResultsList(eventResults);
-            return new CommandResult(String.format(Messages.SEARCH_SUCCESS_MESSAGE, eventResults.size()));
+            return new CommandResult(resultsList(eventResults));
         case assignmentTasks:
             ArrayList<Task> assignmentResults = getSearchQueryAssignments(taskList);
-            printResultsList(assignmentResults);
-            return new CommandResult(String.format(Messages.SEARCH_SUCCESS_MESSAGE, assignmentResults.size()));
+            return new CommandResult(resultsList(assignmentResults));
         default:
             return new CommandResult(String.format(Messages.INVALID_SEARCH_FORMAT));
         }
--- src/test/java/command/SearchTest.java
+++ src/test/java/command/SearchTest.java
@@ -16,6 +16,7 @@ public class SearchTest {
     private static TaskList filledTaskList;
     private static TaskList emptyTaskList;
     private static Ui ui;
+    private StringBuilder searchString;
 
     /**
      * Initialize hard-coded test cases.
@@ -24,7 +25,7 @@ public class SearchTest {
         emptyTaskList = new TaskList();
         filledTaskList = new TaskList();
         ui = new Ui();
-
+        searchString = new StringBuilder();
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
         DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm");
 
@@ -37,18 +38,66 @@ public class SearchTest {
         LocalDateTime testDateTime3 = LocalDateTime.parse(date3, dateTimeFormatter);
         LocalDateTime testDateTime4 = LocalDateTime.parse(date4, dateTimeFormatter);
 
-        Assignment testCaseOne = new Assignment("Assignment 3", "CS2102", testDateTime1, " ");
-        Assignment testCaseTwo = new Assignment("Assignment 5", "CS2102", testDateTime1, " ");
+        Assignment testCaseOne = new Assignment("Assignment 3", "CS2102", testDateTime1, "-");
+        Assignment testCaseTwo = new Assignment("Assignment 5", "CS2102", testDateTime1, "-");
         Assignment testCaseThree = new Assignment("OP1", "CS2101", testDateTime3, "15%");
-        Event testCaseFour = new Event("midterms", "MPSH1A", testDateTime1, testDateTime2, " ");
+        Event testCaseFour = new Event("midterms", "MPSH1A", testDateTime1, testDateTime2, "-");
         Event testCaseFive = new Event("Countdown", "TimeSquare", testDateTime3, testDateTime4, "new year new me");
-        Event testCaseSix = new Event("mid", "MPSH1A", testDateTime1, testDateTime2, " ");
+        Event testCaseSix = new Event("mid", "MPSH1A", testDateTime1, testDateTime2, "-");
         filledTaskList.addTask(testCaseOne);
         filledTaskList.addTask(testCaseTwo);
         filledTaskList.addTask(testCaseThree);
         filledTaskList.addTask(testCaseFour);
         filledTaskList.addTask(testCaseFive);
         filledTaskList.addTask(testCaseSix);
+    }
+
+    private String eventSingleResultString() {
+        searchString.append(Messages.SEARCH_SUCCESS_MESSAGE);
+        searchString.append(System.lineSeparator());
+        searchString.append("  1.[E][X] midterms (at: MPSH1A | Fri 13 Mar 2020 18:00 - 20:30)");
+        searchString.append(System.lineSeparator());
+        searchString.append("            notes: -");
+        searchString.append(System.lineSeparator());
+        return searchString.toString();
+    }
+
+    private String eventMultipleResultsString() {
+        searchString.append(Messages.SEARCH_SUCCESS_MESSAGE);
+        searchString.append(System.lineSeparator());
+        searchString.append("  1.[E][X] midterms (at: MPSH1A | Fri 13 Mar 2020 18:00 - 20:30)");
+        searchString.append(System.lineSeparator());
+        searchString.append("            notes: -");
+        searchString.append(System.lineSeparator());
+        searchString.append("  2.[E][X] mid (at: MPSH1A | Fri 13 Mar 2020 18:00 - 20:30)");
+        searchString.append(System.lineSeparator());
+        searchString.append("            notes: -");
+        searchString.append(System.lineSeparator());
+        return searchString.toString();
+    }
+
+    private String assignmentSingleResultString() {
+        searchString.append(Messages.SEARCH_SUCCESS_MESSAGE);
+        searchString.append(System.lineSeparator());
+        searchString.append("  1.[A][X] Assignment 3 (by: Fri 13 Mar 2020 18:00 | mod: CS2102)");
+        searchString.append(System.lineSeparator());
+        searchString.append("            notes: -");
+        searchString.append(System.lineSeparator());
+        return searchString.toString();
+    }
+
+    private String assignmentMultipleResultsString() {
+        searchString.append(Messages.SEARCH_SUCCESS_MESSAGE);
+        searchString.append(System.lineSeparator());
+        searchString.append("  1.[A][X] Assignment 3 (by: Fri 13 Mar 2020 18:00 | mod: CS2102)");
+        searchString.append(System.lineSeparator());
+        searchString.append("            notes: -");
+        searchString.append(System.lineSeparator());
+        searchString.append("  2.[A][X] Assignment 5 (by: Fri 13 Mar 2020 18:00 | mod: CS2102)");
+        searchString.append(System.lineSeparator());
+        searchString.append("            notes: -");
+        searchString.append(System.lineSeparator());
+        return searchString.toString();
     }
 
     @Test
@@ -68,28 +117,36 @@ public class SearchTest {
     }
 
     @Test
-    public void testSearchExecuteEvent() {
+    public void testSearchExecuteOneEvent() {
         assertEquals(new SearchCommand("midterms", "event").execute(filledTaskList, ui).feedbackToUser,
-                String.format(Messages.SEARCH_SUCCESS_MESSAGE, 1));
-        assertEquals(new SearchCommand("mid", "event").execute(filledTaskList, ui).feedbackToUser,
-                String.format(Messages.SEARCH_SUCCESS_MESSAGE, 2));
+                eventSingleResultString());
     }
 
     @Test
-    public void testSearchExecuteAssignment() {
+    public void testSearchExecuteMultipleEvents() {
+        assertEquals(new SearchCommand("mid", "event").execute(filledTaskList, ui).feedbackToUser,
+                eventMultipleResultsString());
+    }
+
+    @Test
+    public void testSearchExecuteSingleAssignment() {
         assertEquals(new SearchCommand("assignment 3", "assignment").execute(filledTaskList,ui).feedbackToUser,
-                String.format(Messages.SEARCH_SUCCESS_MESSAGE,1));
+                assignmentSingleResultString());
+    }
+
+    @Test
+    public void testSearchExecuteMultipleAssignments() {
         assertEquals(new SearchCommand("assignment", "assignment").execute(filledTaskList,ui).feedbackToUser,
-                String.format(Messages.SEARCH_SUCCESS_MESSAGE,2));
+                assignmentMultipleResultsString());
     }
 
     @Test
     public void testSearchExecute_emptyResults() {
         assertEquals(new SearchCommand("abcd", "event").execute(filledTaskList, ui).feedbackToUser,
-                String.format(Messages.SEARCH_SUCCESS_MESSAGE, 0));
+                Messages.EMPTY_SEARCH_RESULTS_ERROR);
         assertEquals(new SearchCommand("abcd", "assignment").execute(filledTaskList, ui).feedbackToUser,
-                String.format(Messages.SEARCH_SUCCESS_MESSAGE, 0));
+                Messages.EMPTY_SEARCH_RESULTS_ERROR);
         assertEquals(new SearchCommand("abcd", "all").execute(filledTaskList, ui).feedbackToUser,
-                String.format(Messages.SEARCH_SUCCESS_MESSAGE, 0));
+                Messages.EMPTY_SEARCH_RESULTS_ERROR);
     }
 }
