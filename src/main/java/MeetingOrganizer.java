78ab3ab3ccb7d6ece327fb852f645aa656c184a4
lowxizhi <lowxizhi@gmail.com>
2020-03-29 18:28:18 +0800

    restoring git stash

--- src/main/java/MeetingOrganizer.java
+++ src/main/java/MeetingOrganizer.java
@@ -1,17 +1,13 @@
-import exception.InvalidUrlException;
+import command.CommandHandler;
 import exception.MoException;
 import inputparser.CliParser;
-import meeting.Meeting;
 import meeting.MeetingList;
-import modulelogic.LessonsGenerator;
-import schedulelogic.ScheduleHandler;
 import schedulelogic.TeamMember;
 import schedulelogic.TeamMemberList;
 import storage.Storage;
 import ui.TextUI;
 
 import java.io.FileNotFoundException;
-import java.time.LocalTime;
 import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -36,7 +32,7 @@ public class MeetingOrganizer {
             if (myTeamMemberList.getSize() > 0) {
                 myTeamMemberList.getTeamMemberList().forEach(member -> mainUser = member.isMainUser() ? member : null);
             }
-            TextUI.teamMemberListMsg(myTeamMemberList.getTeamMemberList(),mainUser.getName());
+            CommandHandler.listContacts();
         } catch (FileNotFoundException e) {
             TextUI.introMsg();
             TextUI.showLoadingError();
@@ -65,131 +61,56 @@ public class MeetingOrganizer {
         }
 
         switch (userCommand) {
-        case "add using link":
-            int checkerForRepeatedName = 0;
-            checkerForRepeatedName = myTeamMemberList.getTeamMemberList().stream()
-                    .mapToInt(person -> check(person, userInputWords[0])).sum();
-            if (checkerForRepeatedName == 1) {
-                TextUI.showRepeatedPerson(userInputWords[0]);
-                break;
-            }
-
-            member = new TeamMember(userInputWords[0]);
-            String name = userInputWords[0];
-            LessonsGenerator myLessonGenerator;
-            try {
-                myLessonGenerator = new LessonsGenerator(userInputWords[1]);
-                myLessonGenerator.generate();
-                ArrayList<String[]> myLessonDetails = myLessonGenerator.getLessonDetails();
-                for (int k = 0; k < myLessonDetails.size(); k++) {
-                    String startTimeString = null;
-                    String endTimeString = null;
-                    for (int j = 0; j < myLessonDetails.get(k).length; j++) {
-                        switch (j) {
-                        case 0:
-                            startTimeString = myLessonDetails.get(k)[j].substring(0, 2) + ":" + myLessonDetails.get(k)[j].substring(2);
-                            break;
-                        case 1:
-                            endTimeString = myLessonDetails.get(k)[j].substring(0, 2) + ":" + myLessonDetails.get(k)[j].substring(2);
-                            break;
-                        case 2:
-                            startDay = getNumberFromDay(myLessonDetails.get(k)[j]);
-                            endDay = startDay;
-                            break;
-                        case 3:
-                            //future improvement: since myLessonDetails.get(k)[3] contains data on the
-                            // week number that this class occurs on, add capability of schedule to reflect
-                            // schedule of the current week.
-                            break;
-                        default:
-                            //data only has four sections from api
-                            break;
-                        }
-                    }
-                    member.addBusyBlocks(name, startDay, startTimeString, endDay, endTimeString);
-                }
-                if (myTeamMemberList.getSize() == 0) {
-                    mainUser = member;
-                    member.setMainUser();
-                }
-                myTeamMemberList.add(member);
-                TextUI.showAddedMember(member.getName());
-            } catch (InvalidUrlException e) {
-                System.out.println(e.getMessage());
-            }
+        case "add using link": //add new contact with NUSMODS link. <Contact name> <NUSMODS link>
+            commandHandler.addUsingLink(userInputWords, startDay, endDay);
+        break;
+        case "contacts":  //list all contacts. contacts
+            commandHandler.listContacts();
             break;
-        case "contacts":  // contacts
-            TextUI.teamMemberListMsg(myTeamMemberList.getTeamMemberList(), mainUser.getName());
+        case "timetable": //timetable OR timetable <Member Number> OR timetable <Member Number1> <Member Number2>
+            //(eg. timetable 0 1 3)
+            commandHandler.displayTimetable(userInputWords);
             break;
-        case "timetable": // timetable OR timetable <Member Number 1> OR timetable <Member Number 1>
-            // <Member Number 2> (eg. timetable 0 1 3)
-            try {
-                if (userInputWords.length > 1) {
-                    ArrayList<TeamMember> myScheduleList = new ArrayList<TeamMember>();
-                    for (int i = 1; i < userInputWords.length; i++) {
-                        memberNumber = Integer.parseInt(userInputWords[i]);
-                        member = myTeamMemberList.getTeamMemberList().get(memberNumber);
-                        myScheduleList.add(member);
-                    }
-                    ScheduleHandler myScheduleHandler = new ScheduleHandler(myScheduleList);
-                    Boolean[][] myMasterSchedule;
-                    myMasterSchedule = myScheduleHandler.getMasterSchedule();
-                    System.out.println("Timetable of the selected team member/s:");
-                    TextUI.printTimetable(myMasterSchedule);
-                } else {
-                    System.out.println("Your timetable:");
-                    TextUI.printTimetable(mainUser.getSchedule());
-                }
-            } catch (IndexOutOfBoundsException e) {
-                TextUI.indexOutOfBoundsMsg();
-                System.out.println("Index should be within range 0 to " + (myTeamMemberList.getTeamMemberList().size() - 1) + ".");
-                TextUI.teamMemberListMsg(myTeamMemberList.getTeamMemberList(),mainUser.getName());
-            } catch (NumberFormatException e) {
-                TextUI.invalidNumberTimetableMsg();
-            }
-
+        case "schedule": //schedule a meeting. schedule <Meeting Name> <Start Day> <Start Time> <End Day> <End Time>
+            //(eg. schedule meeting 3 17:00 3 19:00)
+            commandHandler.scheduleMeeting(userInputWords);
             break;
-        case "schedule": //schedule <Meeting Name> <Start Day> <Start Time> <End Day> <End Time> (eg. schedule meeting 3 17:00 3 19:00)
-            String meetingName = userInputWords[1];
-            startDay = Integer.parseInt(userInputWords[2]);
-            LocalTime startTime = LocalTime.parse(userInputWords[3]);
-            endDay = Integer.parseInt(userInputWords[4]);
-            LocalTime endTime = LocalTime.parse(userInputWords[5]);
-
-            try {
-                if (ScheduleHandler.isValidMeeting(mainUser, startDay, startTime, endDay, endTime)) {
-                    Meeting myMeeting = new Meeting(meetingName, startDay, startTime, endDay, endTime);
-                    myMeetingList.add(myMeeting);
-                    mainUser.addBusyBlocks(meetingName, startDay, userInputWords[3], endDay, userInputWords[5]);
-                    TextUI.meetingListSizeMsg(myMeetingList);
-                } else {
-                    System.out.println("Schedule is blocked at that timeslot");
-                }
-            } catch (MoException e) {
-                System.out.println(e.getMessage() + ", try again.");
-            }
-            // Replace main user's timetable with updated meeting blocks into TeamMember.TeamMemberList for storage purposes.
-            myTeamMemberList.set(0, mainUser);
+        case "delete": //delete a meeting slot. delete <Meeting Number>
+            commandHandler.deleteMeeting(userInputWords[1]);
             break;
-        case "delete":
-            int index = Integer.parseInt(userInputWords[1]) - 1;
-            try {
-                Meeting meetingToDelete = myMeetingList.getMeetingList().get(index);
-                String meetingNameToDelete = meetingToDelete.getMeetingName();
-                mainUser.deleteBlocksWithName(meetingNameToDelete);
-                myMeetingList.delete(index);
-                myTeamMemberList.set(0, mainUser);
-            } catch (IndexOutOfBoundsException e) {
-                TextUI.displayInvalidDeleteTarget();
-            }
-            break;
-        case "meetings": //list all current meeting slots
-            TextUI.listMeetings();
-            myMeetingList.show();
+        case "meetings": //list all scheduled meeting slots. meetings
+            commandHandler.listMeetings();
             break;
         default:
             throw new MoException("Unknown command, please try again.");
         }
+    }
+
+    private void listMeetings() {
+        commandHandler.listMeetings();
+    }
+
+    private void deleteMeeting(String userInputWord) {
+        commandHandler.deleteMeeting(userInputWord);
+    }
+
+    private void scheduleMeeting(String[] userInputWords) {
+
+        // Replace main user's timetable with updated meeting blocks into TeamMember.TeamMemberList for storage purposes.
+        commandHandler.scheduleMeeting(userInputWords);
+    }
+
+    private void displayTimetable(String[] userInputWords) {
+        commandHandler.displayTimetable(userInputWords);
+    }
+
+    private void listContacts() {
+        commandHandler.listContacts();
+    }
+
+    private void addUsingLink(String[] userInputWords, Integer startDay, Integer endDay) {
+
+        commandHandler.addUsingLink(userInputWords, startDay, endDay);
     }
 
     private int check(TeamMember person, String name) {
