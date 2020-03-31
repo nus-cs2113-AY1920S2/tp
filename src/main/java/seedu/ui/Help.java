e4e48b952bebc6f094aa0191b81499ac429bc6a1
lowjiayee <lowjiayee123@gmail.com>
2020-03-31 16:18:41 +0800

    Replace System.out.println to display

--- src/main/java/seedu/ui/Help.java
+++ src/main/java/seedu/ui/Help.java
@@ -3,27 +3,27 @@ package seedu.ui;
 public class Help extends UI {
 
     public void printGetHelp() {
-        System.out.println("Hello, please select the type of "
+        display("Hello, please select the type of "
                 + "command that you wish to get the format for.");
-        System.out.println("1. Event");
-        System.out.println("2. Attendance");
-        System.out.println("3. Performance");
-        System.out.println("4. Student List");
-        System.out.println("To track any list, input: type_of_list list");
-        System.out.println("Note: All command are NOT case sensitive.");
+        display("1. Event");
+        display("2. Attendance");
+        display("3. Performance");
+        display("4. Student List");
+        display("To track any list, input: type_of_list list");
+        display("Note: All command are NOT case sensitive.");
     }
 
     public void printEventHelp() {
-        System.out.print("To add an event, use the following format:\n\t"
+        display("To add an event, use the following format:\n\t"
                 + "Event add n/Event_name v/Venue_name d/yyyy-MM-dd. "
                 + "You may also replace 'Event' with one of the following type:"
                 + "\n\t- Seminar\n\t- Exam\n\t- Tutorial\n\n");
-        System.out.print("To edit an event, use the following format:\n\t"
+        display("To edit an event, use the following format:\n\t"
                 + "Event editDateTime i/index_of_Event, or\n\t"
                 + "Event editName i/index_of_Event, or\n\t"
                 + "Event editVenue i/index_of_Event, or\n\t"
                 + "Event editEvent (please edit these lines)\n\n");
-        System.out.print("To edit an event, use the following format:\n\t"
+        display("To edit an event, use the following format:\n\t"
                 + "Event editDateTime i/index_of_Event, or\n\t"
                 + "Event editName i/index_of_Event, or\n\t"
                 + "Event editVenue i/index_of_Event, or\n\t"
@@ -31,24 +31,24 @@ public class Help extends UI {
     }
 
     public void printPerformanceHelp() {
-        System.out.println("All performance list should exist under an existing event."
+        display("All performance list should exist under an existing event."
                 + "Follow step-by-step commands after the following commands are input.");
-        System.out.println("To add students' performance, input:\n\t"
+        display("To add students' performance, input:\n\t"
                 + "performance add\n");
-        System.out.println("To delete a student's performance under an event, input:\n\t"
+        display("To delete a student's performance under an event, input:\n\t"
                 + "Performance delete\n");
-        System.out.println("To sort a student's performance list under an event, input:\n\t"
+        display("To sort a student's performance list under an event, input:\n\t"
                 + "Performance sort\n");
     }
 
     public void printAttendanceHelp() {
-        System.out.println("All attendance list should exist under an existing event."
+        display("All attendance list should exist under an existing event."
                 + "Follow step-by-step commands after the following commands are input.");
-        System.out.println("To add students' attendance, input:\n\t"
+        display("To add students' attendance, input:\n\t"
                 + "Attendance add\n");
-        System.out.println("To delete a student's attendance, input:\n\t"
+        display("To delete a student's attendance, input:\n\t"
                 + "Attendance delete\n");
-        System.out.println("To sort a student's attendance list, input:\n\t"
+        display("To sort a student's attendance list, input:\n\t"
                 + "Attendance sort\n");
     }
 
