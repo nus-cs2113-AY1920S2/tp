1392e43e357c3e6a94b8616e7abda0d80feef019
Nyan Wun Paing <nwp.nyan@gmail.com>
2020-04-01 21:39:29 +0800

    Fix new bugs on Add command

--- src/main/java/seedu/happypills/logic/parser/PatientParser.java
+++ src/main/java/seedu/happypills/logic/parser/PatientParser.java
@@ -116,18 +116,23 @@ public class PatientParser {
                 input = "@" + input;
                 updates = input.split(" /");
             }
+            System.out.println(updates[0]);
             for (String update : updates) {
                 if (update.trim().startsWith("n") && parseInput[0].equalsIgnoreCase("")) {
-                    parseInput[0] = update.trim().substring(1);
-                } else if (update.trim().startsWith("ic") && parseInput[1].equalsIgnoreCase("")) {
-                    parseInput[1] = update.trim().substring(2).toUpperCase();
-                } else if (update.trim().startsWith("p") && (parseInput[2].equalsIgnoreCase("")
-                        || !isInteger(parseInput[2].trim()))) {
-                    parseInput[2] = update.trim().substring(1);
-                } else if (update.trim().startsWith("dob") && parseInput[3].equalsIgnoreCase("")) {
-                    parseInput[3] = update.trim().substring(3);
-                } else if (update.trim().startsWith("b") && parseInput[4].equalsIgnoreCase("")) {
-                    parseInput[4] = update.trim().substring(1);
+                    parseInput[0] = update.substring(1).trim();
+                } else if (update.trim().startsWith("ic") && (parseInput[1].equalsIgnoreCase(""))
+                        || !checkNric(parseInput[1].trim())) {
+                    System.out.println(update);
+                    parseInput[1] = update.trim().substring(2).toUpperCase().trim();
+                } else if (update.trim().startsWith("p") && ((parseInput[2].equalsIgnoreCase("")
+                        || !isInteger(parseInput[2].trim()) || !checkPhoneNum(parseInput[2].trim())))) {
+                    parseInput[2] = update.substring(1).trim();
+                } else if (update.trim().startsWith("dob") && (parseInput[3].equalsIgnoreCase("")
+                        || !checkDate(parseInput[3].trim()))) {
+                    parseInput[3] = update.trim().substring(3).trim();
+                } else if (update.trim().startsWith("b") && (parseInput[4].equalsIgnoreCase(""))
+                        || !checkType(parseInput[4].trim())){
+                    parseInput[4] = update.trim().substring(1).trim();
                 }
             }
         }
@@ -193,6 +198,8 @@ public class PatientParser {
      */
     static boolean checkNric(String nric) {
         String pattern = "([S-T][0-9][0-9][0-9][0-9][0-9][0-9][0-9][A-Z])";
+        System.out.println(nric);
+        System.out.println(nric.matches(pattern));
         return nric.matches(pattern);
     }
 
