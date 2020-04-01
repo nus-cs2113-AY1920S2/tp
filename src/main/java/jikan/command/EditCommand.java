d538e4aee57f6fd6ef2d392e30b3717bfd40d96a
nigellenl <nigelle.nl@gmail.com>
2020-04-02 01:03:13 +0800

    Fix edit command exception handling

--- src/main/java/jikan/command/EditCommand.java
+++ src/main/java/jikan/command/EditCommand.java
@@ -13,6 +13,7 @@ import jikan.ui.Ui;
 
 import java.io.IOException;
 import java.time.Duration;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -38,66 +39,51 @@ public class EditCommand extends Command {
             int tagDelim = parameters.indexOf("/et");
             int allocDelim = parameters.indexOf("/ea");
 
-            // edit name
-            if (delimiter != -1 && tagDelim == -1 && allocDelim == -1)  {
-                Parser.activityName = parameters.substring(0, delimiter).strip();
-
-                //edit tag
-            } else if (delimiter == -1 && tagDelim != -1 && allocDelim == -1) {
-                Parser.activityName = parameters.substring(0, tagDelim).strip();
-
-                //edit allocatedTime
-            } else {
-                Parser.activityName = parameters.substring(0, allocDelim).strip();
-            }
-
-            if (Parser.activityName.isEmpty()) {
-                throw new EmptyNameException();
-            }
-
-            int index = activityList.findActivity(Parser.activityName);
             String newName = "";
             String[] tmpTags;
             String tmpAlloc = "";
             Duration newAllocTime = null;
             Set<String> newTags = new HashSet<String>();
 
-            //edit name
-            if (delimiter != -1 && tagDelim == -1 && allocDelim == -1) {
-                newName = parameters.substring(delimiter + 4);
+            if (delimiter != -1)  {
                 Parser.activityName = parameters.substring(0, delimiter).strip();
+                newName = parameters.substring(delimiter + 4).strip();
 
-                //edit tag
-            } else if (delimiter == -1 && tagDelim != -1 && allocDelim == -1) {
+            } else if (tagDelim != -1) {
+                Parser.activityName = parameters.substring(0, tagDelim).strip();
                 tmpTags = (parameters.substring(tagDelim + 4).split(" "));
-                for (String t : tmpTags) {
-                    newTags.add(t);
-                }
-
-                //edit allocatedTime
-            } else {
+                newTags.addAll(Arrays.asList(tmpTags));
+            } else if (allocDelim != -1) {
+                Parser.activityName = parameters.substring(0, allocDelim).strip();
                 tmpAlloc = parameters.substring(allocDelim + 4);
                 try {
                     newAllocTime = parseDuration(tmpAlloc);
                 } catch (InvalidTimeFrameException e) {
                     Ui.printDivider("Invalid time frame entered!");
                 }
+            } else {
+                throw new InvalidEditFormatException();
             }
+
+            if (Parser.activityName.isEmpty()) {
+                Ui.printDivider("Activity name cannot be empty!");
+                throw new EmptyNameException();
+            }
+
+            int index = activityList.findActivity(Parser.activityName);
 
             if (index != -1) {
                 if (!(newName.isEmpty() && newTags.isEmpty() && tmpAlloc.isEmpty())) {
                     if (!newName.isEmpty()) {
                         activityList.updateName(index, newName);
-                    }
-                    if (!newTags.isEmpty()) {
+                    } else if (!newTags.isEmpty()) {
                         if (existsInTag(activityList.get(index).getTags())) {
                             throw new ExistingTagGoalException();
                         } else {
                             activityList.updateTags(index, newTags);
                         }
-
-                    }
-                    if (!tmpAlloc.isEmpty()) {
+                    } else {
+                        assert newAllocTime != null;
                         if (newAllocTime.isNegative()) {
                             throw new NegativeDurationException();
                         } else {
@@ -105,8 +91,13 @@ public class EditCommand extends Command {
                         }
                     }
                 } else {
-                    // no new details provided
-                    throw new InvalidEditFormatException();
+                    if (newName.isEmpty()) {
+                        Ui.printDivider("New activity name cannot be empty!");
+                        throw new EmptyNameException();
+                    } else {
+                        // no new details provided
+                        throw new InvalidEditFormatException();
+                    }
                 }
                 Ui.printDivider("Activity named " + Parser.activityName + " has been updated!");
 
@@ -118,10 +109,10 @@ public class EditCommand extends Command {
             Ui.printDivider("No activity with this name exists!");
             Log.makeInfoLog("Edit command failed as there was no such activity saved.");
         } catch (EmptyNameException e) {
-            Ui.printDivider("Activity name cannot be empty!");
+            //Ui.printDivider("Activity name cannot be empty!");
             Log.makeInfoLog("Edit command failed as there was no activity name provided.");
-        } catch (StringIndexOutOfBoundsException | InvalidEditFormatException e) {
-            Ui.printDivider("New details not provided!");
+        } catch (StringIndexOutOfBoundsException | ArrayIndexOutOfBoundsException | InvalidEditFormatException e) {
+            Ui.printDivider("Incorrect edit command format entered!");
             Log.makeInfoLog("Edit command failed as there was no updated activity detail provided.");
         } catch (ExistingTagGoalException e) {
             Ui.printDivider("Tag cannot be edited as there is an existing tag goal!");
@@ -131,6 +122,7 @@ public class EditCommand extends Command {
             Log.makeInfoLog("Edit command failed as there was an error in loading the tag file.");
         } catch (NegativeDurationException e) {
             Ui.printDivider("Please enter a positive target time!");
+            Log.makeInfoLog("Edit command failed as a negative target time was provided.");
         }
     }
 
