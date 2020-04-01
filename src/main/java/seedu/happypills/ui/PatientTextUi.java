289973a3f1c1630fe20151d0bb05be7431cc0d44
Nyan Wun Paing <nwp.nyan@gmail.com>
2020-04-01 22:59:42 +0800

    Fix delete bug

--- src/main/java/seedu/happypills/ui/PatientTextUi.java
+++ src/main/java/seedu/happypills/ui/PatientTextUi.java
@@ -9,7 +9,7 @@ import java.util.Map;
 
 public class PatientTextUi extends TextUi {
 
-    public static String patientNotFoundMessage = "    The patient cannot be found. Please try again.\n" + DIVIDER;
+    public static String patientNotFoundMessage = "    The patient cannot be found. Please try again.\n";
     public static String emptyPatientListMessage = "    There are no patients in the list.\n" + DIVIDER;
 
     /**
