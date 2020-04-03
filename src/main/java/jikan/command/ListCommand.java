a4e2bba0520745dc0c6f97cabb60a8ff69ab8e52
btricec <btricec@users.noreply.github.com>
2020-04-03 15:46:15 +0800

    Refactor list command and fix issue #187

--- src/main/java/jikan/command/ListCommand.java
+++ src/main/java/jikan/command/ListCommand.java
@@ -2,20 +2,17 @@ package jikan.command;
 
 import jikan.activity.Activity;
 import jikan.activity.ActivityList;
+import jikan.exception.ExtraParametersException;
+import jikan.exception.NameTooLongException;
 import jikan.ui.Ui;
 
 import jikan.exception.InvalidTimeFrameException;
 
-import java.text.ParseException;
-import java.time.DayOfWeek;
-import java.time.LocalDate;
-import java.time.Month;
-import java.time.YearMonth;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.time.temporal.TemporalAdjusters;
 import java.util.Calendar;
-import java.util.Date;
 
 import static jikan.Jikan.lastShownList;
 
@@ -40,11 +37,7 @@ public class ListCommand extends Command {
     public void executeCommand(ActivityList activityList) {
         // If no time frame is specified, print the entire list
         if (parameters == null) {
-            lastShownList.activities.clear();
-            Ui.printList(activityList);
-
-            // Can't do lastShownList = activityList, otherwise we just copy
-            lastShownList.activities.addAll(activityList.activities);
+            listAll(activityList);
         } else {
             try {
                 listInterval(activityList);
@@ -53,18 +46,29 @@ public class ListCommand extends Command {
                         + "Or use day / week / month to view tasks in the respective time period.");
             } catch (InvalidTimeFrameException e) {
                 Ui.printDivider("Please enter a valid time frame; the end date must come after the start date.");
+            } catch (ExtraParametersException e) {
+                Ui.printDivider("Extra parameters detected!\n"
+                        + "Use day / week / month to view tasks in the respective time period.\"");
             }
         }
     }
 
+    private void listAll(ActivityList activityList) {
+        lastShownList.activities.clear();
+        Ui.printList(activityList);
+
+        // Can't do lastShownList = activityList, otherwise we just copy
+        lastShownList.activities.addAll(activityList.activities);
+    }
+
     private void listInterval(ActivityList activityList)
-            throws InvalidTimeFrameException, DateTimeParseException {
+            throws InvalidTimeFrameException, DateTimeParseException, ExtraParametersException {
         String[] listInputs;
         listInputs = parameters.split(" ", 2);
 
         lastShownList.activities.clear();
 
-        LocalDate startDate = null;
+        LocalDate startDate;
         LocalDate endDate = null;
 
         // Parse either format
@@ -73,10 +77,17 @@ public class ListCommand extends Command {
         // Check if the user has given a verbal input
         // (User can either say day or daily and get the same output)
         switch (listInputs[0]) {
+        case "today":
+            // Fallthrough
         case "day":
             // Fallthrough
         case "daily":
+            checkExtraParameters(listInputs);
             startDate = LocalDate.now();
+            break;
+        case "yesterday":
+            checkExtraParameters(listInputs);
+            startDate = LocalDate.now().minusDays(1);
             break;
         case "week":
             // Fallthrough
@@ -89,7 +100,6 @@ public class ListCommand extends Command {
             } else {
                 startDate = LocalDate.now();
             }
-
             // Set current Monday and Sunday as time range
             startDate = startDate.with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY));
             endDate = startDate.with(TemporalAdjusters.nextOrSame(DayOfWeek.SUNDAY));
@@ -101,9 +111,7 @@ public class ListCommand extends Command {
             // Otherwise get current date
             if (listInputs.length == 2) {
                 try {
-                    Month month = Month.valueOf(listInputs[1].toUpperCase());
-                    YearMonth yearMonth = YearMonth.of(Calendar.getInstance().get(Calendar.YEAR), month.getValue());
-                    startDate = yearMonth.atDay(1);
+                    startDate = getMonth(listInputs[1]);
                 } catch (IllegalArgumentException e) {
                     Ui.printDivider("Please specify the full month name.");
                     return;
@@ -117,6 +125,7 @@ public class ListCommand extends Command {
             endDate = startDate.with(TemporalAdjusters.lastDayOfMonth());
             break;
         default:
+            // date / date range is given
             startDate = LocalDate.parse(listInputs[0], parser);
 
             if (listInputs.length == 2) {
@@ -124,7 +133,10 @@ public class ListCommand extends Command {
             }
             break;
         }
+        printList(activityList, startDate, endDate);
+    }
 
+    private void printList(ActivityList activityList, LocalDate startDate, LocalDate endDate) throws InvalidTimeFrameException {
         // Only one date is specified; return all entries with start date coinciding with that date
         if (endDate == null) {
             for (Activity i : activityList.activities) {
@@ -146,6 +158,20 @@ public class ListCommand extends Command {
                 }
             }
             Ui.printList(lastShownList);
+        }
+    }
+
+    private LocalDate getMonth(String listInput) {
+        LocalDate startDate;
+        Month month = Month.valueOf(listInput.toUpperCase());
+        YearMonth yearMonth = YearMonth.of(Calendar.getInstance().get(Calendar.YEAR), month.getValue());
+        startDate = yearMonth.atDay(1);
+        return startDate;
+    }
+
+    private void checkExtraParameters(String[] listInputs) throws ExtraParametersException {
+        if (listInputs.length > 1) {
+            throw new ExtraParametersException();
         }
     }
 }
