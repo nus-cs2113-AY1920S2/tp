85d9e4ce5b16500e8c900031b377f58bd6c54f92
A11riseforme <ycl0217@hotmail.com>
2020-04-04 17:57:10 +0800

    update ModuleLoader to make it download moduleList and store in data folder

--- src/main/java/seedu/nuke/data/storage/StoragePath.java
+++ src/main/java/seedu/nuke/data/storage/StoragePath.java
@@ -1,7 +1,8 @@
 package seedu.nuke.data.storage;
 
 public class StoragePath {
-    public static final String TASK_FILE_DIRECTORY_PATH = "data/files";
-    public static final String NUS_MODULE_LIST_PATH = "moduleList.json";
-    public static final String SAVE_PATH = "data/save.txt";
+    public static final String FILE_BASE_PATH = "data/";
+    public static final String TASK_FILE_DIRECTORY_PATH = FILE_BASE_PATH + "files";
+    public static final String NUS_MODULE_LIST_PATH = FILE_BASE_PATH + "moduleList.json";
+    public static final String SAVE_PATH = FILE_BASE_PATH + "save.txt";
 }
