6b132a0bd423b4aaa8731900e99127c8e392097c
siuhian <42374522+siuhian@users.noreply.github.com>
2020-04-06 02:51:16 +0800

    Debugged start command

--- src/main/java/jikan/command/StartCommand.java
+++ src/main/java/jikan/command/StartCommand.java
@@ -9,8 +9,11 @@ import jikan.parser.Parser;
 import jikan.ui.Ui;
 
 
+import java.time.DateTimeException;
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -20,6 +23,10 @@ import java.util.Scanner;
 public class StartCommand extends Command {
 
     private Scanner scanner;
+    private boolean hasTarget = false;
+    private boolean hasTag = false;
+    private boolean hasTargetAndTag = false;
+    private static final int maxActivityLength = 25;
 
     /**
      * Constructor to create a new start command.
@@ -35,10 +42,16 @@ public class StartCommand extends Command {
         if (hasStarted) {
             stopExecution();
         } else {
-            continueExecution();
+            assert Parser.tags.isEmpty();
+            continueExecution(activityList);
         }
     }
 
+    /**
+     * Checks if there is a concurrently running activity.
+     * @param startTime a LocalDateTime object to check if parser is waiting for a running activity.
+     * @return true if there is a concurrent running activity and false otherwise.
+     */
     private boolean hasStarted(LocalDateTime startTime) {
         if (startTime != null) {
             return true;
@@ -47,13 +60,20 @@ public class StartCommand extends Command {
         }
     }
 
+    /**
+     * Stops executing current activity if a concurrent running activity is found.
+     */
     private void stopExecution() {
         String line = Parser.activityName + " is ongoing!";
         Log.makeInfoLog("Could not start activity due to already ongoing activity.");
         Ui.printDivider(line);
     }
 
-    private void continueExecution() {
+    /**
+     * Continues execution as no concurrent running activity is found.
+     * @param activityList a list of tracked activities.
+     */
+    private void continueExecution(ActivityList activityList) {
         boolean commandIsEmpty = hasParameter(this.parameters);
         String activityName = "";
         if (commandIsEmpty) {
@@ -64,10 +84,15 @@ public class StartCommand extends Command {
         if (activityName.isEmpty()) {
             Ui.printDivider("Activity name cannot be empty");
         } else {
-            System.out.println(activityName);
+            checkActivity(activityName, activityList);
         }
     }
 
+    /**
+     * Check if the start command is empty.
+     * @param parameters the parameters to start command.
+     * @return true if it is empty and false otherwise.
+     */
     private boolean hasParameter(String parameters) {
         if (parameters.isEmpty()) {
             return true;
@@ -75,6 +100,11 @@ public class StartCommand extends Command {
         return false;
     }
 
+    /**
+     * Get activity name from parameters to start command.
+     * @param parameters the parameters to start command.
+     * @return activity name of the activity to be started.
+     */
     private String parseActivityName(String parameters) {
         String scenario;
         String activityName;
@@ -101,18 +131,34 @@ public class StartCommand extends Command {
         return activityName;
     }
 
+    /**
+     * Method to check for tags and allocated time.
+     * @param tagDelimiter index where tag flag is found.
+     * @param allocateDelimiter index where allocate flag is found.
+     * @return a string with information about whether tags and allocated time are found.
+     */
     private String getScenario(int tagDelimiter, int allocateDelimiter) {
         if (tagDelimiter != -1 && allocateDelimiter != -1) {
+            this.hasTargetAndTag = true;
             return "hasTagAndTarget";
         } else if (tagDelimiter != -1) {
+            this.hasTag = true;
             return "hasTagOnly";
         } else if (allocateDelimiter != -1) {
+            this.hasTarget = true;
             return "hasTargetOnly";
         } else {
             return "hasNoTagAndTarget";
         }
     }
 
+    /**
+     * Method to extract activity name.
+     * @param parameters parameters to start command.
+     * @param tagDelimiter index where tag flag is found.
+     * @param allocateDelimiter index where allocate flag is found.
+     * @return activity name
+     */
     private String handleTagAndTarget(String parameters, int tagDelimiter, int allocateDelimiter) {
         String activityName = "";
         int delimiter = 0;
@@ -130,6 +176,12 @@ public class StartCommand extends Command {
         }
     }
 
+    /**
+     * Method to extract activity name.
+     * @param parameters parameters to start command.
+     * @param delimiter index where tag flag or allocate flag is found.
+     * @return activity name.
+     */
     private String handleTagOrTarget(String parameters, int delimiter) {
         String activityName = parameters.substring(0, delimiter);
         activityName = activityName.trim();
@@ -140,51 +192,152 @@ public class StartCommand extends Command {
         }
     }
 
-
-
-    /*@Override
-    public void executeCommand(ActivityList activityList) {
-        if (Parser.startTime != null) {
-            String line = Parser.activityName + " is ongoing!";
-            Log.makeInfoLog("Could not start activity due to already ongoing activity.");
-            Ui.printDivider(line);
+    /**
+     * Method to check if the activity exists in activitylist and does not exceed 25 characters.
+     * @param activityName the string representing activity name.
+     * @param activityList a list of tracked activities.
+     */
+    private void checkActivity(String activityName, ActivityList activityList) {
+        int index = activityList.findActivity(activityName);
+        if (index != -1) {
+            Ui.printDivider("There is already an activity with this name. Would you like to continue it?");
+            continueActivity(activityList, scanner, index);
+        } else if (activityName.length() > maxActivityLength) {
+            Log.makeInfoLog("Activity name longer than 25 characters");
+            Ui.printDivider("Please input an activity name that is shorter than 25 characters.");
         } else {
-            // tags should be reset
-            assert Parser.tags.isEmpty();
-            int tagDelimiter = parameters.indexOf("/t");
-            int allocateDelimiter = parameters.indexOf("/a");
-
-            // check if there exists an activity with this name
-            String activityName = getActivityName(tagDelimiter,allocateDelimiter);
-
-            if (activityName.length() > 25) {
-                Log.makeInfoLog("Activity name longer than 25 characters");
-                Ui.printDivider("Please input an activity name that is shorter than 25 characters.");
-                return;
-            }
-
-            Parser.activityName = activityName;
-            int index = activityList.findActivity(activityName);
-            if (index != -1) {
-                Ui.printDivider("There is already an activity with this name. Would you like to continue it?");
-                continueActivity(activityList, scanner, index);
-            } else {
-                try {
-                    parseActivity(tagDelimiter,allocateDelimiter);
-                    Parser.startTime = LocalDateTime.now();
-                    Log.makeFineLog("Started: " + activityName);
-                    Ui.printDivider("Started: " + activityName);
-                } catch (WrongDateFormatException w) {
-                    Log.makeInfoLog("Wrong format for allocated time.");
-                    Ui.printDivider("Please input in this format HH/MM/SS");
-                } catch (EmptyNameException e) {
-                    Ui.printDivider("Activity name cannot be empty!");
-                } catch (EmptyTagException e) {
-                    Ui.printDivider("Tags cannot be empty!");
-                }
-            }
+            addActivityToList(activityName);
         }
-    }*/
+    }
+
+    /**
+     * Method to add a activity to the activity list.
+     * @param activityName the string representing activity name.
+     */
+    private void addActivityToList(String activityName) {
+        if (hasTargetAndTag) {
+            parseActivityWithBothField(activityName, this.parameters);
+        } else if (hasTarget) {
+            addActivityWithTarget(activityName, this.parameters);
+        } else if (hasTag) {
+            addActivityWithTag(activityName, this.parameters);
+        } else {
+            addActivity(activityName);
+        }
+    }
+
+    /**
+     * Parse the tag and allocate information.
+     * @param activityName the string representing activity name.
+     * @param line a line with information on tags and allocated time.
+     */
+    private void parseActivityWithBothField(String activityName, String line) {
+        int allocateIndex = line.indexOf("/a");
+        int tagIndex = line.indexOf("/t");
+        String tagInfo = "";
+        String durationInfo = "";
+        if (tagIndex < allocateIndex) {
+            tagInfo = line.substring(tagIndex, allocateIndex);
+            durationInfo = line.substring(allocateIndex);
+            tagInfo = tagInfo.trim();
+            durationInfo = durationInfo.trim();
+        } else if (allocateIndex < tagIndex) {
+            durationInfo = line.substring(allocateIndex, tagIndex);
+            tagInfo = line.substring(tagIndex);
+            tagInfo = tagInfo.trim();
+            durationInfo = durationInfo.trim();
+        }
+        durationInfo = durationInfo.substring(2);
+        durationInfo = durationInfo.trim();
+        tagInfo = tagInfo.substring(2);
+        tagInfo = tagInfo.trim();
+        addActivityWithBothField(durationInfo, tagInfo, activityName);
+    }
+
+    /**
+     * Add activity with both tag and allocate time.
+     * @param durationInfo information about the duration.
+     * @param tagInfo information about the tag.
+     * @param activityName the string representing activity name.
+     */
+    private void addActivityWithBothField(String durationInfo, String tagInfo, String activityName) {
+        LocalTime startTime = LocalTime.MIN;
+        LocalTime endTime;
+        String[] tagString = tagInfo.split(" ");
+        try {
+            endTime = LocalTime.parse(durationInfo);
+            Duration allocatedTime = Duration.between(startTime, endTime);
+            if (tagString.length > 2) {
+                Ui.printDivider("Cannot have more than 2 tags");
+            } else if (allocatedTime == Duration.parse("PT0S")) {
+                Ui.printDivider("Please provide a non zero allocated time");
+            } else {
+                Parser.tags.addAll(Arrays.asList(tagString));
+                Parser.allocatedTime = allocatedTime;
+                addActivity(activityName);
+            }
+        } catch (DateTimeException e) {
+            Ui.printDivider("Time provided is invalid, please provide time in this format"
+                    + " HH:MM:SS");
+        }
+    }
+
+    /**
+     * Add activity with allocated time only.
+     * @param activityName the string representing activity name.
+     * @param line a line with information about allocated time.
+     */
+    private void addActivityWithTarget(String activityName, String line) {
+        int index = line.indexOf("/a");
+        String durationInfo = line.substring(index + 2);
+        durationInfo = durationInfo.trim();
+        LocalTime startTime = LocalTime.MIN;
+        LocalTime endTime;
+        try {
+            endTime = LocalTime.parse(durationInfo);
+            Duration allocatedTime = Duration.between(startTime, endTime);
+            if (allocatedTime == Duration.parse("PT0S")) {
+                Ui.printDivider("Please provide a non zero allocated time");
+            } else {
+                Parser.allocatedTime = allocatedTime;
+                addActivity(activityName);
+
+            }
+        } catch (DateTimeException e) {
+            Ui.printDivider("Time provided is invalid, please provide time in this format"
+                    + " HH:MM:SS");
+        }
+    }
+
+    /**
+     * Add activity with tags only.
+     * @param activityName the string representing activity name.
+     * @param line a line with information about tags.
+     */
+    private void addActivityWithTag(String activityName, String line) {
+        int index = line.indexOf("/t");
+        String tagInfo = line.substring(index + 2);
+        tagInfo = tagInfo.trim();
+        String[] tagString = tagInfo.split(" ");
+        if (tagString.length > 2) {
+            Ui.printDivider("Cannot have more than 2 tags");
+        } else {
+            Parser.tags.addAll(Arrays.asList(tagString));
+            addActivity(activityName);
+        }
+    }
+
+    /**
+     * Add activity to activity list.
+     * @param activityName the string representing activity name.
+     */
+    private void addActivity(String activityName) {
+        Parser.activityName = activityName;
+        Parser.startTime = LocalDateTime.now();
+        Log.makeFineLog("Started: " + activityName);
+        Ui.printDivider("Started: " + activityName);
+    }
+
 
     /**
      * Received user input on whether or not to continue the activity.
@@ -192,7 +345,7 @@ public class StartCommand extends Command {
      * @param activityList List of activities.
      * @param scanner Parse user input.
      */
-    /*private static void continueActivity(ActivityList activityList, Scanner scanner, int index) {
+    private static void continueActivity(ActivityList activityList, Scanner scanner, int index) {
         String userInput = scanner.nextLine();
         if (userInput.equalsIgnoreCase("yes") || userInput.equalsIgnoreCase("y")) {
             Parser.activityName = activityList.get(index).getName();
@@ -205,77 +358,5 @@ public class StartCommand extends Command {
             Parser.activityName = null;
             Ui.printDivider("Okay then, what else can I do for you?");
         }
-    }*/
-
-    /**
-     * Parses the started activity for name and tags.
-     *
-     * @param tagDelimiter the index of the tag delimiter.
-     * @param allocateDelimiter the index of the allocation delimiter.
-     */
-    /*private void parseActivity(int tagDelimiter, int allocateDelimiter) throws EmptyNameException,
-            WrongDateFormatException, EmptyTagException {
-        String activityName = getActivityName(tagDelimiter, allocateDelimiter);
-        if (activityName.isBlank()) {
-            throw new EmptyNameException();
-        }
-        String activityInfo;
-        if (tagDelimiter != -1 || allocateDelimiter != -1) {
-            activityInfo = this.parameters.substring(activityName.length() + 1);
-        } else {
-            return;
-        }
-        if (allocateDelimiter != -1) {
-            String allocatedTime;
-            try {
-                activityInfo = activityInfo.substring(3);
-                int index = activityInfo.indexOf(" ");
-                if (index != -1) {
-                    allocatedTime = activityInfo.substring(0,index);
-                    activityInfo = activityInfo.substring(index + 1);
-                } else {
-                    allocatedTime = activityInfo;
-                }
-                parseDuration(allocatedTime);
-            } catch (WrongDateFormatException | StringIndexOutOfBoundsException w) {
-                throw new WrongDateFormatException();
-            }
-        }
-        if (tagDelimiter != -1) {
-            try {
-                activityInfo = activityInfo.substring(3);
-                String [] tagString = activityInfo.split(" ");
-                Parser.tags.addAll(Arrays.asList(tagString));
-            } catch (StringIndexOutOfBoundsException e) {
-                throw new EmptyTagException();
-            }
-
-        }
-        //return "Started: " + Parser.activityName;
-    }*/
-
-    /*private String getActivityName(int tagDelimiter, int allocateDelimiter) {
-        if (tagDelimiter != -1) {
-            return this.parameters.substring(0,tagDelimiter).strip();
-        } else if (allocateDelimiter != -1) {
-            return this.parameters.substring(0,allocateDelimiter).strip();
-        } else {
-            return this.parameters;
-        }
-
-    }*/
-
-    /*private void parseDuration(String allocatedTime) throws WrongDateFormatException {
-        String[] tokenizedInputs;
-        tokenizedInputs = allocatedTime.split("/");
-        if (tokenizedInputs.length != 3) {
-            throw new WrongDateFormatException();
-        } else {
-            String hours = tokenizedInputs[0] + "H";
-            String minutes = tokenizedInputs[1] + "M";
-            String seconds = tokenizedInputs[2] + "S";
-            String duration = "PT" + hours + minutes + seconds;
-            Parser.allocatedTime = Duration.parse(duration);
-        }
-    }*/
+    }
 }
