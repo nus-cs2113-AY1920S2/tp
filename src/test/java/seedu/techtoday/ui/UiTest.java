3b87ec3388dec25f2604099ffbfc39fc3e74cdd4
melylopez99 <mlopez99@bu.edu>
2020-04-06 00:55:06 -0700

    adding one more test

---
+++ src/test/java/seedu/techtoday/ui/UiTest.java
@@ -0,0 +1,34 @@
+package seedu.techtoday.ui;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class UiTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getCommand_DeleteArticle1_success() {
+        String input = "delete article 1";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        String actualOutput = Ui.getCommand();
+        assertEquals("delete article 1", actualOutput);
+    }
+
+    @Test
+    void getCommand_DeleteWithSpaces_success() {
+        String input = "delete                   ";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        String actualOutput = Ui.getCommand();
+        assertEquals("delete", actualOutput);
+        }
+}
