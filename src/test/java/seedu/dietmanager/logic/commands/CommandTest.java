7068c82f39ac215304f9dc445c690424cd65bba2
Yuxiang <yuxianglim1227@gmail.com>
2020-04-07 23:14:59 +0800

    Added Command Test

---
+++ src/test/java/seedu/dietmanager/logic/commands/CommandTest.java
@@ -0,0 +1,44 @@
+package seedu.dietmanager.logic.commands;
+
+import org.junit.jupiter.api.Test;
+import seedu.dietmanager.commons.exceptions.InvalidFormatException;
+import seedu.dietmanager.logic.Result;
+import seedu.dietmanager.model.Food;
+import seedu.dietmanager.model.Profile;
+import seedu.dietmanager.ui.UI;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+class CommandTest {
+
+    @Test
+    void setWeightCommand() {
+        Profile profile = new Profile();
+        UI ui = new UI();
+        profile.setProfile("John",20, "Male", 180, 80, 75);
+        Command command = new SetWeightCommand("set-weight","50");
+        command.execute(profile, ui);
+        assertEquals("Your weight has been changed to 50.00.", command.resultString);
+    }
+
+    @Test
+    void deleteWeightCommand() throws InvalidFormatException {
+        Profile profile = new Profile();
+        UI ui = new UI();
+        profile.setProfile("John",20, "Male", 180, 80, 75);
+        Command command = new DeleteWeightCommand("delete-weight","1");
+        command.execute(profile, ui);
+        assertEquals("Weight Record: 80.0kg  has been removed successfully!", command.resultString);
+    }
+
+    @Test
+    void checkWeightRecordCommand() throws InvalidFormatException {
+        Profile profile = new Profile();
+        UI ui = new UI();
+        profile.setProfile("John",20, "Male", 180, 80, 75);
+        Command command = new CheckWeightRecordCommand("check-weight-progress", "John");
+        command.execute(profile, ui);
+        assertEquals("No Pain No Gain! You have not lost weight yet! Strive on!", command.resultString);
+    }
+}
