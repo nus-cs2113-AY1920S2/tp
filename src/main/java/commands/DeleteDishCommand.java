6dcfb23ac4e83a23ad62667b9e35d85e6298d21b
gmuthu17 <gmuthu@berkeley.edu>
2020-04-06 21:53:03 -0400

    major progress in fixing add and delete error handling

--- src/main/java/commands/DeleteDishCommand.java
+++ src/main/java/commands/DeleteDishCommand.java
@@ -1,15 +1,45 @@
 package commands;
 
+import exceptions.DishIngredientsMissingException;
+import exceptions.DishNameMissingException;
+import exceptions.DishPriceMissingException;
+import exceptions.InvalidAddDishCommandException;
+import exceptions.InvalidDeleteDishCommandException;
 import menu.Menu;
 
 public class DeleteDishCommand extends Menu {
 
     /**
      * Removes dish from menu.
-     * @param name name of dish to remove
+     * @param input name of dish to remove
      */
-    public static void deleteDish(String name) {
-        Menu.getDishMap().remove(name);
+    public static void deleteDish(String input) {
+        try {
+            checkFormat(input);
+            String name = AddDishCommand.parseName(input);
+            if (Menu.getDishMap().isEmpty()) {
+                System.out.println("There are no dishes to delete!");
+            } else if (!Menu.getDishMap().containsKey(name)) {
+                System.out.println("Dish " + name + " does not exist!");
+            } else {
+                Menu.getDishMap().remove(name);
+                System.out.println("Dish " + name + " successfully remove!");
+            }
+        } catch (DishNameMissingException e) {
+            System.out.println("Must include name of dish to delete!");
+        } catch (InvalidDeleteDishCommandException | StringIndexOutOfBoundsException e) {
+            System.out.println("Invalid delete dish command!");
+            System.out.println("The correct format is: delete dish; n/NAME;");
+        }
+
+    }
+
+     public static void checkFormat(String input) throws DishNameMissingException, InvalidDeleteDishCommandException {
+        if (!input.contains("n/")) {
+            throw new DishNameMissingException();
+        } else if (input.chars().filter(ch -> ch == ';').count() != 2) {
+            throw new InvalidDeleteDishCommandException();
+        }
     }
 
 }
