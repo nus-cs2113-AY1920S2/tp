58331c3fef974e4f2eb361b4deebb158c73a5616
weiyang <piadruids@gmail.com>
2020-04-09 12:34:11 +0800

    fixed issue

--- src/main/java/common/Messages.java
+++ src/main/java/common/Messages.java
@@ -16,7 +16,8 @@ public class Messages {
     public static final String MESSAGE_INVALID_MEETING_RANGE = "Range entered is not valid.";
     public static final String MESSAGE_INVALID_NUMBER = "You have entered an invalid number.";
     public static final String MESSAGE_INVALID_NUSMODLINK = "You have entered an invalid nusmods link!";
-    public static final String MESSAGE_MODULECODE_IN_BLACKLIST = " Module's information from NUSMOD API is ill-formatted.";
+    public static final String MESSAGE_MODULECODE_IN_BLACKLIST = " module information unable to be captured from API, please key it in"
+                                                                    + " manually using [edit] command.";
     public static final String MESSAGE_EMPTY_MODULE = "Module either can't be fetched from NUSMOD API or has been deprecated from NUS";
     public static final String MESSAGE_RETURN_SUCCESS = "SUCCESS";
     public static final String MESSAGE_INDEX_OUT_OF_BOUNDS = "You have entered an index out of bounds.";
