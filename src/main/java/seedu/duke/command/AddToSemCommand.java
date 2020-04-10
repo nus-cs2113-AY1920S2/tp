282c2376b369c2f45420a98c0cd9b534316ab53f
bennychanya <bennychanyongan@live.com.sg>
2020-04-10 21:59:05 +0800

    Edited Check Method to allow Module to be added if user failed the selected module Changed condition for thrown exception to allow 0 MC module Changed condition for thrown exception to allow modules up to 20MC

--- src/main/java/seedu/duke/command/AddToSemCommand.java
+++ src/main/java/seedu/duke/command/AddToSemCommand.java
@@ -4,6 +4,7 @@ import seedu.duke.exception.RuntimeException;
 import seedu.duke.data.AvailableModulesList;
 
 import seedu.duke.data.SemesterList;
+import seedu.duke.module.Grading;
 import seedu.duke.module.SelectedModule;
 import seedu.duke.ui.Ui;
 
@@ -32,8 +33,8 @@ public class AddToSemCommand extends AddCommand {
      * @throws RuntimeException throws when meets a runtime exception.
      */
     private void addModule(SemesterList semesterList) throws RuntimeException {
-        boolean isModuleExist = checkModuleExist(semesterList);
-        if (isModuleExist) {
+        boolean doesModuleExistAndNotFailed = checkModuleExistAndNotFailed(semesterList);
+        if (doesModuleExistAndNotFailed) {
             if (selectedModule.isIdValid() && selectedModule.isNameValid()) {
                 throw new RuntimeException(String.format("ID <%s> or name <%s> is already exist in your semester list!",
                         selectedModule.getId(), selectedModule.getName()));
@@ -46,9 +47,9 @@ public class AddToSemCommand extends AddCommand {
             }
         }
 
-        if (selectedModule.getModuleCredit() <= 0) {
-            throw new RuntimeException("The module should not have non-positive module credit");
-        } else if (selectedModule.getModuleCredit() > 50) {
+        if (selectedModule.getModuleCredit() < 0) {
+            throw new RuntimeException("The module should not have negative module credit");
+        } else if (selectedModule.getModuleCredit() > 20) {
             throw new RuntimeException("Are you sure the module is that large? :O");
         }
 
@@ -79,14 +80,25 @@ public class AddToSemCommand extends AddCommand {
     }
 
     /**
-     *Allows users to add to data later(or not) and then it if users add a module to data,
-     *it will automatically update the information of the selected module.
-     * @param semesterList user's current semester list
+     * Checks if the selected module being added is exist in any of the semester list
+     * If selected module exist inside semester list, it will check if the module inside the list has failed.
+     * It will return false to allow the method to be added into semester list.
+     * @param semesterList user's semester list
      * @return boolean value of true if the module is in the user's semester list, and false otherwise
      */
-    private boolean checkModuleExist(SemesterList semesterList) {
+    private boolean checkModuleExistAndNotFailed(SemesterList semesterList) {
         for (SemModulesList sem: semesterList) {
-            if (sem.isInList(selectedModule.getName()) || sem.isInList(selectedModule.getId())) {
+            for (SelectedModule module: sem) {
+                boolean hasSameModuleId = module.getId().equals(selectedModule.getId());
+                boolean hasSameModuleName = module.getName().equals(selectedModule.getName());
+                if (hasSameModuleId || hasSameModuleName) {
+                    boolean isModuleGradeF = module.getGrade().equals(Grading.F);
+                    boolean isModuleGradeCU = module.getGrade().equals(Grading.CU);
+                    boolean hasModuleFailed = isModuleGradeCU || isModuleGradeF;
+                    if (hasModuleFailed) {
+                        return false;
+                    }
+                }
                 return true;
             }
         }
