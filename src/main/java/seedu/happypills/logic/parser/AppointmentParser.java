0dfc4ae696c8c5f9ac736b1167e5626267505a17
sitinadiah25 <sitinadiah333@gmail.com>
2020-04-10 17:34:05 +0800

    Update fix

--- src/main/java/seedu/happypills/logic/parser/AppointmentParser.java
+++ src/main/java/seedu/happypills/logic/parser/AppointmentParser.java
@@ -72,6 +72,7 @@ public class AppointmentParser extends Parser {
                 || !Checker.isValidNric(parseInput[0].trim())) {
             printMissingInput(parseInput);
             String input = promptUser().trim();
+            System.out.println(TextUi.DIVIDER);
             if (input.equalsIgnoreCase("clear")) {
                 throw new HappyPillsException(Messages.MESSAGE_COMMAND_ABORTED);
             }
